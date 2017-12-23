92835e686bee219f2518fc802a65cf2bca23a688
hneth <hansneth@gmail.com>
2017-12-23 22:37:36 +0100

    minor edits (to test functionality)

--- _app/server.R
+++ _app/server.R
@@ -1,42 +1,41 @@
-
-# Define server logic
+# Define server logic:
 library(shiny)
 library(shinyBS)
 
-
 shinyServer(
-    function(input, output, session) {
-        
-        
-        ######
-        # Underlying data structure
-
-       
-
-        
-        
-        #####
-        # Outputs
-        
-        # outputs for tab on raw data table
-        output$rawdatatable <- DT::renderDataTable(DT::datatable({
-            cars
-        }))
-        
-        
-        # outputs for tab on confusion table
-        output$confusiontable <- renderTable({ matrix(data = c(25, 130, 2500, 240.892), 
-                                                      nrow = 2, byrow = TRUE,
-                                                      dimnames = list(c("A", "B"), c("C", "D"))) },  
-                                             bordered = TRUE,  
-                                             hover = TRUE,  
-                                             align = 'c',  
-                                             digits = 2, 
-                                             rownames = TRUE,
-                                             na = 'missing')  
-        
-        output$mosaicplot <- renderPlot(mosaicplot(table(1:5, 5:1)))
-
-        
-    }
+  function(input, output, session) {
+    
+    ## Inputs used in all representations:
+    # N <- input$N       # N in population
+    # prev <- input$prev # prevalence in population = p(true positive)
+    # sens <- input$sens # sensitivity = p(positive decision | true positive)
+    # spec <- input$spec # specificity = p(negative decision | true negative)
+    
+    ## Define common data structure:
+    # population as df
+    
+    ## Outputs:
+    
+    # outputs for tab on raw data table: 
+    output$rawdatatable <- DT::renderDataTable(DT::datatable({
+      cars
+    }))
+    
+    # outputs for confusion table:
+    output$confusiontable <- renderTable({ matrix(data = c(25, 130, 2500, 240.892), 
+                                                  nrow = 2, byrow = TRUE,
+                                                  dimnames = list(c("A", "B"), c("C", "D"))) },  
+                                         bordered = TRUE,  
+                                         hover = TRUE,  
+                                         align = 'c',  
+                                         digits = 2, 
+                                         rownames = TRUE,
+                                         na = 'missing')  
+    
+    # outputs for mosaic plot:
+    output$mosaicplot <- renderPlot(mosaicplot(table(1:5, 5:1)))
+    
+    # tree of natural frequencies:
+    # icon array:
+  }
 )
