c7b035dc6cfda16092c54b85bf2df80b180c1c77
hneth <hansneth@gmail.com>
2018-01-08 10:49:12 +0100

    minor edits (add "predicted condition" as possible alternative of "decision")

--- R/init_txt.R
+++ R/init_txt.R
@@ -10,8 +10,8 @@
 ## Set defaults for all text inputs (txt):
 
 txt <- list(
-  scenario.lbl = "Scenario name",
-  scenario.text = "Describe the current scenario in a short paragraph of text here.\nThis description may include several sentences.",
+  scenario.lbl    = "Scenario name",
+  scenario.text   = "Describe the current scenario in a short paragraph of text here.\nThis description may include several sentences.",
   scenario.source = "Describe the source information for this scenario here",
 
   target.population.lbl = "Describe the target population in a few words here",
@@ -21,21 +21,21 @@ txt <- list(
   cond.true.lbl  = "Condition true",  # "has condition", "is affected"
   cond.false.lbl = "Condition false", # "does not have condition", "is unaffected"
 
-  ## (b) Decision:
-  decision.lbl  = "Decision",          # "diagnostic decision", "Mammography", "HIV test"
-  dec.true.lbl  = "Decision positive", # "is called", "is judged to have condition", "is classified as affected"
-  dec.false.lbl = "Decision negative", # "is not called", "is judged to not have condition", "is classified as unaffected"
+  ## (b) Decision/predicted condition:
+  decision.lbl  = "Decision",          # "Predicted condition", "diagnostic decision", "Mammography", "HIV test"
+  dec.true.lbl  = "Decision positive", # "Prediction positive", "called", "is judged to have condition", "is classified as affected"
+  dec.false.lbl = "Decision negative", # "Prediction negative", "not called", "is judged to not have condition", "is classified as unaffected"
 
   ## (c) Labels for the 4 SDT cases (combinations):
-  sdt.hi.lbl = "True positive",  # "hit", "true positive", "has condition and is detected as such", "set(dec POS & cond TRUE)"
-  sdt.mi.lbl = "False negative", # "miss", "false negative", "omission", "has condition and is NOT detected as such", "set(dec NEG & cond TRUE)"
+  sdt.hi.lbl = "True positive",  # "hit", "true positive", "has condition and is detected/predicted as such", "set(dec POS & cond TRUE)"
+  sdt.mi.lbl = "False negative", # "miss", "false negative", "omission", "has condition and is NOT detected/predicted as such", "set(dec NEG & cond TRUE)"
   sdt.fa.lbl = "False positive", # "false alarm",       "false positive", "set(dec POS & cond FALSE)"
   sdt.cr.lbl = "True negative"   # "correct rejection", "true negative",  "set(dec NEG & cond FALSE)"
   )
 
 ## ToDo:
-## - add txt to cus object
-## - make txt user-customizable
+## - add txt to cus object?
+## - make txt user-customizable!
 
 ## -----------------------------------------------
 ## eof.
