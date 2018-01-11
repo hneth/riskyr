119182e27469dfc58063e72cc32b64b3a59124ec
nigradwohl <nico.gradwohl@uni.kn>
2018-01-11 13:09:51 +0100

    updated fuction to plot icon arrays

--- R/plot_iconarray.R
+++ R/plot_iconarray.R
@@ -11,13 +11,49 @@
 
 ## - ...
 
-dev.new(width = 5, height = 5)
+dev.new(width = 5, height = 5)  # create device with known aspect ratio.
 
-plot(4, 4, type = "n", xlim = c(0, 4), ylim = c(0, 4))
+# define parameters:
+pop <- 73 # define population size.
+ncols <- 10  # define number of columns (to be filled by row)
 
-rect(1, 1, 2, 2, asp = 1)
+nrows <- ceiling(pop / ncols)
 
-points(3, 3, pch = 15)
+# create raw plot to accomodate the icons:
+plot(1, 1, type = "n", xlim = c(0.5, 10 + 0.5), ylim = c(0.5, 10 + 0.5))
+
+# rect(0.55, 0.45, 1.45, 1.45, asp = 1)  # lower left.
+#
+# rect(1.55, 0.45, 2.45, 1.45, asp = 1)  # to the right of first.
+#
+# rect(2.55, 0.45, 3.45, 1.45, asp = 1)
+#
+# rect(3.55, 0.45, 4.45, 1.45, asp = 1)
+#
+# rect(0.45, 1.55, 1.45, 2.55, asp = 1)  # above first.
+
+
+design.matrix <- expand.grid(1:ncols, 10:(1 + ncols - nrows))  # create a matrix for positions.
+design.matrix <- design.matrix[1:pop, ]  # truncate the matrix to population size.
+
+# TODO: Integrate color in design matrix?
+
+for(i in 1:nrow(design.matrix)){
+
+  xleft <- design.matrix[i,1] - 0.45
+  xright <- design.matrix[i,1] + 0.45
+
+  ybottom <- design.matrix[i,2] - 0.45
+  ytop <- design.matrix[i,2] + 0.45
+
+  rect(xleft, ybottom, xright, ytop, asp = 1)
+}
+
+# points(3, 3, pch = 15)  # does not really work with cex...; maybe try later.
+
+lines(par("usr")[1], par("usr")[2], col = "red")
+
+lines(x = c(1, 5), y = c(5, 5), col = "red")
 
 ## -----------------------------------------------
 ## eof.
