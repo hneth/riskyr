67a020bcc40e1388d7a9bc8957ac04c307408ec2
hneth <hansneth@gmail.com>
2018-01-11 09:56:47 +0100

    minor edit

--- R/temp_code.R
+++ R/temp_code.R
@@ -2,6 +2,8 @@
 ## 2018 01 11
 ## -----------------------------------------------
 ## State function: What is this code for?
+## This file serves as a template for code
+## in the /R folder.
 
 ## code
 
