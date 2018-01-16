d30dd2cc363da35dda621d2ac5a20c5449ba9915
hneth <hansneth@gmail.com>
2018-01-16 13:53:21 +0100

    use basic colors

--- R/init_pal.R
+++ R/init_pal.R
@@ -9,6 +9,32 @@
 ## -----------------------------------------------
 ## Set defaults for all color inputs (pal):
 
+## Utility function:
+
+makeTransparent = function(..., alpha = .50) {
+
+  if (alpha < 0 | alpha > 1) {
+    stop("alpha must be between 0 and 1")
+  }
+
+  alpha <- floor(255 * alpha)
+  newColor <- col2rgb(col = unlist(list(...)), alpha = FALSE)
+
+  .makeTransparent <- function(col, alpha) {
+    rgb(red = col[1],
+        green = col[2],
+        blue=col[3],
+        alpha = alpha, maxColorValue = 255)
+  }
+
+  newColor <- apply(newColor, 2, .makeTransparent, alpha = alpha)
+
+  return(newColor)
+
+}
+
+
+
 ## -----------------------------------------------
 ## (1) Define some named colors:
 
@@ -40,7 +66,32 @@
   col.orange.1 <- rgb(247, 169, 127, max = 255)
   col.orange.2 <- rgb(242, 100, 24, max = 255)
 
+  ## (3) basic colors + transparency:
+  my.red  <- "tomato3"
+  my.blue <- "steelblue3"
+  my.green <- "olivedrab4"
+
+  my.yellow <- "lightgoldenrod1"
+  my.orange <- "sienna1"
+
+  green.1 <- makeTransparent(my.green, alpha = .50)
+  green.2 <- makeTransparent(my.green, alpha = 1.0)
+
+  red.1 <- makeTransparent(my.red, alpha = .50)
+  red.2 <- makeTransparent(my.red, alpha = 1.0)
+
+  blue.1 <- makeTransparent(my.blue, alpha = .50)
+  blue.2 <- makeTransparent(my.blue, alpha = 1.0)
+
+  yellow.1  <- makeTransparent(my.yellow, alpha = .50)
+  yellow.2  <- makeTransparent(my.yellow, alpha = 1.0)
+
+  orange.1  <- makeTransparent(my.orange, alpha = .50)
+  orange.2  <- makeTransparent(my.orange, alpha = 1.0)
+
 }
+
+
 
 ## -----------------------------------------------
 ## (2) Select and name some colors by their function
@@ -51,26 +102,26 @@
   col.N <- grey(.95, .99) # "white", col.grey.1
 
   ## (b) Define 2 colors for condition cases:
-  col.true <- "lightgoldenrod1" # "gold1", "gold", col.orange.1, "yellow2"
-  col.false <- "lightskyblue2" # "deepskyblue1" # "lightskyblue2" # col.blue.1
+  col.true <- my.yellow # "lightgoldenrod1" "gold1", col.orange.1, "yellow2"
+  col.false <- "lightskyblue2" #, my.blue, "deepskyblue1" # "lightskyblue2" # col.blue.1
   ## Combine:
   cond.colors <- setNames(c(col.true, col.false),
                          c("true", "false")
   )
 
   ## (c) Define 4 colors for SDT cases:
-  col.hi <- "olivedrab4" # "palegreen4" # col.green.2
-  col.mi <- "tomato3" # "orangered3" # "firebrick3", col.red.2
-  col.fa <- "tomato1" # "orangered1" # "firebrick1", col.red.1
-  col.cr <- "olivedrab3" # "springgreen2" # "palegreen3" # col.green.1
+  col.hi <- my.green # "olivedrab4", "palegreen4", col.green.2
+  col.mi <- my.red   # "tomato3", "orangered3", "firebrick3", col.red.2
+  col.fa <- "lightsalmon2" # lightcoral" # "tomato1" # "orangered1" # "firebrick1", col.red.1
+  col.cr <- "olivedrab3"   # "springgreen2" # "palegreen3" # col.green.1
   ## Combine:
   sdt.colors <- setNames(c(col.hi, col.mi, col.fa, col.cr),
                          c("hi", "mi", "fa", "cr")
                          )
 
   ## (d) Define 2 colors for PVs:
-  col.ppv <- "sienna1" # col.orange.2 # "orange3" "firebrick" "red3"
-  col.npv <- "steelblue3" # col.blue.3, "green4" "gray50" "brown4" "chartreuse4"
+  col.ppv <- my.orange # "sienna1" # col.orange.2 # "orange3" "firebrick" "red3"
+  col.npv <- my.blue # "steelblue3", col.blue.3, "green4" "gray50" "brown4" "chartreuse4"
 }
 
 ## -----------------------------------------------
