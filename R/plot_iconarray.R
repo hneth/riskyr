eb33cb656aca18103e63faa8cd58b5d3c911f8dd
nigradwohl <nico.gradwohl@uni.kn>
2018-01-24 11:16:01 +0100

    added ordered arrays to plot_iconarray

--- R/plot_iconarray.R
+++ R/plot_iconarray.R
@@ -18,12 +18,20 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# define parameters:
-pop <- 111 # define population size.
-ncols <- 10  # define number of columns (to be filled by row)
-nrows_fix <- 10  # fix number of rows (for each block) to some value.
+# Note: The function finally only needs:
+  # A vector of identities (colors.)
+    # This can be obtained in different ways (e.g., calculation by respective function)
+  # A vector of positions (to be generated according to version.)
+  # A number of blocks.
+  # A size for the icons (cex)
+  # ...?
 
-nrows <- ceiling(pop / ncols)  # calculate the required number of rows (for one block).
+# old parameters:
+
+# ncols <- 10  # define number of columns (to be filled by row)
+# nrows_fix <- 10  # fix number of rows (for each block) to some value.
+#
+# nrows <- ceiling(pop / ncols)  # calculate the required number of rows (for one block).
 
 # source("./R/init_txt.R")
 # source("./R/init_pal.R")
@@ -31,25 +39,39 @@ nrows <- ceiling(pop / ncols)  # calculate the required number of rows (for one 
 # source("./R/comp_freq.R")
 # source("./R/comp_popu.R")
 
-popu
-
-sdt.colors
-
-N <- freq$N
-
-freq <- comp_freq(N = pop)
+# popu
+#
+# sdt.colors
+#
+# N <- freq$N
+#
+# freq <- comp_freq(N = pop)
 # freq
 
 
 ## (c) SDT (status decision/truth):
+# calculate to be independent from source
+pop <- 100 # define population size.
+sens <- 0.80
+spec <- 0.90
+prev <- 0.70
 
-n.hi <- freq$hi
-n.mi <- freq$mi
-n.fa <- freq$fa
-n.cr <- freq$cr
+n.hi <- pop * prev * sens
+n.mi <- 14 # pop * prev * (1 - sens)
+n.fa <- 3 # pop * (1 - prev) * (1 - spec)
+n.cr <- pop * (1 - prev) * spec
+
+length(rep("hi", n.hi))
+length(rep("mi", 14))  # too short???  Apparently a failure in updating...
+length(rep("fa", 3))  # too short???
+length(rep("cr", n.cr))
 
 sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
   rep("fa", n.fa), rep("cr", n.cr))
+
+length(sdt)
+
+sum(n.hi, n.mi, n.fa, n.cr)
 
 # icon_colors <- c(rep(sdt.colors["hi"], n.hi), rep(sdt.colors["mi"], n.mi),
 #                 rep(sdt.colors["fa"], n.fa), rep(sdt.colors["cr"], n.cr))
@@ -85,8 +107,10 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
   ident_vec <- sdt
 
   # option 1 to obtain colors:
-  icon_colors <- c(rep(sdt.colors["hi"], n.hi), rep(sdt.colors["mi"], n.mi),
-                   rep(sdt.colors["fa"], n.fa), rep(sdt.colors["cr"], n.cr))
+  sdt.colors <- c("green", "red", "orange", "blue")
+  # final function takes any four colors mapped onto four identities.
+  icon_colors <- c(rep(sdt.colors[1], n.hi), rep(sdt.colors[2], n.mi),
+                   rep(sdt.colors[3], n.fa), rep(sdt.colors[4], n.cr))
 
   icon_colors <- adjustcolor(icon_colors, alpha.f = .66) # make transparent
 
@@ -97,7 +121,7 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
 
   for (i in ident_types) {
     # replace the identities with their respective colors:
-    icon_colors2[ident_vec == i] <- color_types[i]
+    icon_colors2[ident_vec == i] <- color_types[ident_types == i]
   }
 
   # sum(icon_colors != icon_colors2)  # test for all equal.
@@ -108,8 +132,10 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
   plotx_dim <- c(0, 1)
   ploty_dim <- c(0, 1)  # assuming square plot x = y.
 
-# --------------------------------------------------------------------------
-  # A1 Random position, random colors:
+  # TODO: Each of the types is a potential function.  Then it is more modular!
+
+# A1 Random position, random colors:---------------------------------------
+  #
     # 1) Define positions:
       # 1a) draw random positions:
       posx_vec <- runif(n = pop, min = plotx_dim[1], max = plotx_dim[2])
@@ -133,15 +159,15 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
              # visual details:
              pch = pch, col = grey(.10, .66), bg = col_vec, cex = cex, bty = "o")
 
-# --------------------------------------------------------------------------
-  # A2 Random position, clustered colors:
+# A2 Random position, clustered colors: ---------------------------------------
+  #
     # 1) Define positions:
     # 1a) draw random positions:
       posx_vec <- runif(n = pop, min = plotx_dim[1], max = plotx_dim[2])
       posy_vec <- runif(n = pop, min = ploty_dim[1], max = ploty_dim[2])
 
       # 1b) sort dependent on parameter:
-      type_sort <- "equal"  # options:
+      type_sort <- "mosaic"  # options:
       # right: from left to right, top: from top to bottom,
       # equal: in equal spaces of the plot, mosaic: relative to area.
 
@@ -233,14 +259,21 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
         comp_p <- type_n / sum(type_n)  # proportion in each compartment.
 
         prev <- comp_p["hi"] + comp_p["mi"]  # prevalence (or number of true conditions).
-        p_corr <- comp_p["hi"] + comp_p["cr"]  # proportion of yes-responses.
+        # TODO: Allow split in both directions!
+        # TODO: Rename comp_p.
 
-        # Quadrant dimensions:
-        comp1 <- c(0, p_corr, 0, prev)
-        comp2 <- c(p_corr, 1, 0, prev)
-        comp3 <- c(p_corr, 1, prev, 1)
-        comp4 <- c(0, p_corr, prev, 1)
+        # define boundaries:
+        b1 <- comp_p ["hi"] / (comp_p ["hi"] + comp_p["mi"])
+        b2 <- comp_p ["cr"] / (comp_p ["cr"] + comp_p["fa"])
 
+
+        # Quadrant dimensions (with prevalence in y-direction):
+        comp1 <- c(0, b1, 0, prev)
+        comp2 <- c(b1, 1, 0, prev)
+        comp3 <- c(b2, 1, prev, 1)
+        comp4 <- c(0, b2, prev, 1)
+
+        # TODO: Allow to shuffle components around (using a list?).
         # TODO: not general yet!  How to make it general?  Calculation of area proportions?
 
         # reset vectors:
@@ -248,7 +281,7 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
         posy_vec <- NULL
 
         # set distance parameter:
-        block_d <- 0.000  # this parameter may not be half the size of the distance between min and max.
+        block_d <- 0.02  # this parameter may not be half the size of the distance between min and max.
         # for the example of prevalence == 0.15 it may not exceed 0.075.
 
         # diff(comps)
@@ -280,21 +313,134 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
       }
     }
 
+# A4 Fixed position clustered colors:---------------------------------------
+  #
+
+  # 0. define nrows, ncols, and blocks (with option to calculate them later as well as blocks).
+  nrows <- 10
+  ncols <- 10
+  blocks <- 4
+  col_blocks <- 3
+  row_blocks <- 2
+  block_size <- 5  # maybe generalize in block cols and rows.
+  block_d <- 0.4  # same name as above?
+
+  # 1. Create matrix of positions:
+    design.matrix <- expand.grid(0:(ncols  - 1), (nrows - 1):0)  # create a matrix for positions.
+    design.matrix <- design.matrix[1:pop, ]  # truncate the matrix to population size.
+    # TODO: Can likely be ommitted in favor of the below solution.
+
+    # Get block breakpoints:
+    block_breaks_col <- seq(block_size, by = block_size, length.out = col_blocks - 1)
+    block_breaks_row <- seq(block_size, by = block_size, length.out = row_blocks - 1)
+
+  # 2. Define block membership to determine order:
+    # 2.1. Row-wise filling:
+
+    # For two x two blocks:
+      # y >= block_size: block 1 or 2 (thinking in rows)
+      block_1_2 <- design.matrix$Var2 >= block_size
+      # x >= block size: block 2 or 4
+      block_2_4 <- design.matrix$Var1 >= block_size
+
+      block_vec <- ifelse(block_1_2 & !block_2_4, 1,
+             ifelse(block_1_2 & block_2_4, 2,
+                    ifelse(!block_1_2 & !block_2_4, 3, 4))
+             )
+
+    # For x times y blocks:
+      blocks <- expand.grid(1:col_blocks, 1:row_blocks)
+      blocks <- cbind(blocks, block_num = 1:nrow(blocks))
+
+      blocks <- expand.grid(block_breaks_col, block_breaks_row)
+
+    # use workaround for sorting for the moment (as order and sort do not work as expected):
+    order_vec <- 1:length(block_vec)
+    block_order <- rep(NA, length(block_vec))
+    for (i in unique(block_vec)) {
+      i_log <- block_vec == i  # get logical vector of all 1st block entries.
+      len <- sum(i_log)  # get the respective length.
+      block_order[i_log] <- order_vec[1:len]  # take the first len elements from the order vec.
+      order_vec <- order_vec[(len + 1):length(order_vec)]  # remove the used elements.
+    }
+
+    # sort colors accordingly:
+    col_vec <- col_vec[block_order]
+
+  # 3. Define positions:
+    minx <- min(design.matrix$Var1)
+    maxx <- max(design.matrix$Var1)
+    miny <- min(design.matrix$Var2)
+    maxy <- max(design.matrix$Var2)
+
+    # find adjustment parameter for distances:
+    max_posx <- max(design.matrix$Var1) - block_d
+      # now find a monotonically increasing sequence, resulting in exactly this endpoint.
+    adj_posx <- seq(0, max_posx, length.out = ncols)
+    design.matrix$Var1 <- adj_posx
+
+    max_posy <- max(design.matrix$Var2) - block_d
+    # now find a monotonically increasing sequence, resulting in exactly this endpoint.
+    adj_posy <- seq(max_posy, 0, length.out = nrows)
+    design.matrix$Var2 <- rep(adj_posy , each = nrows)
+
+    # TODO: To make it more general one will likely have to include a factor on block_d following n.blocks - 1.
+
+    # # adjustment sequences:
+    # adjx <- rep(
+    #   seq(1, max(design.matrix$Var1)), length.out = length(design.matrix$Var1[!design.matrix$Var1 %in% c(minx)])) *
+    #   block_d
+    #
+    # # adjy <- rep(
+    # #   seq(1, max(design.matrix$Var2)), each = block_size,
+    # #   length.out = length(design.matrix$Var2[!design.matrix$Var2 %in% c(miny)])) *
+    # #   block_d
+    #
+    # design.matrix$Var1[!design.matrix$Var1 %in% c(minx)] <-
+    #   design.matrix$Var1[!design.matrix$Var1 %in% c(minx)] - adjx
+    # design.matrix$Var2[!design.matrix$Var2 %in% c(maxy)] <-
+    #   design.matrix$Var2[!design.matrix$Var2 %in% c(maxy)] + adjy
+
+    block_size_adj <- block_size - block_d  # block size needs to be adjusted for comparison.
+
+    # calculate block borders:
+    block_size_adjx <- adj_posx[block_size + 1]
+    block_size_adjy <- adj_posy[block_size + 1]
+
+    # design.matrix <- design.matrix / 1.2
+    design.matrix$Var1[design.matrix$Var1 >= (block_size_adjx)] <-
+      design.matrix$Var1[design.matrix$Var1 >= (block_size_adjx)] + block_d
+
+    design.matrix$Var2[design.matrix$Var2 > (block_size_adjy)] <-
+      design.matrix$Var2[design.matrix$Var2 > (block_size_adjy)] + block_d
 
 
+    # save into respective vectors and norm on 0,1 space.
+    posx_vec <- design.matrix$Var1 / (ncols - 1)
+    posy_vec <- design.matrix$Var2 / (nrows - 1)
+
+    # TODO: Not in region anymore --> change plot dimensions or decrease standard distance.
+
+    # Plotting dimensions for testing:
+    plotx_dim <- c(-0.5, 1.5)
+    ploty_dim <- c(-0.5, 1.5)
+
+# ---------------------------------------------------------------------------
+  # B Actual plotting:
     # checking for duplicates:
-    (pos_duplicates <- sum(duplicated(cbind(posx_vec, posy_vec))))
+    (pos_duplicates <- sum(duplicated(round(cbind(posx_vec, posy_vec), 4))))
     # no duplicated coordinates.
 
     # 2) Randomize vector:
-    col_vec <- icon_colors
+    # col_vec <- icon_colors
 
     # 3) Plot:
-    plot(x = 1, xlim = plotx_dim, ylim = ploty_dim, type = "n")
+    plot(x = 1, xlim = plotx_dim, ylim = ploty_dim, type = "n", xlab = "",
+         ylab = "")
 
     # 3a) set plotting character:
     pch <- 22  # filled square as default.
-    cex <- 2
+    cex <- 3
 
     test <- paste(round(posx_vec, 1), round(posy_vec, 1))
 
@@ -303,7 +449,23 @@ sdt <- c(rep("hi", n.hi), rep("mi", n.mi),
            pch = pch, col = grey(0, .66), bg = col_vec, cex = cex, bty = "o")
 
     # optional: add ablines.
+    abline(v = c(0, 1), h = c(0, 1))
 
+
+
+# ---------------------------------------------------------------
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Test functionality of plotting icons:
+    par("pin")  # this is likely the appropriate value to scale cex...
+
+    plot(1, 1, type = "n", xlim = c(0.5, 2.5), ylim = c(0.5, 2.5))
+
+    cex <- max(par("pin")) / length(c(1, 2, 1, 2))
+
+    points(c(1, 2, 1, 2), c(1, 1, 2, 2), pch = 15, cex = cex)  # points are independent of the size of the plotting device!
+
+    design.matrix <- expand.grid(1:ncols, 10:(1 + ncols - nrows))  # create a matrix for positions.
+    design.matrix <- design.matrix[1:pop, ]  # truncate the matrix to population size.
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -457,15 +619,6 @@ system.time({
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Test functionality of plotting icons:
-
-plot(1, 1, type = "n", xlim = c(0.5, 10 + 0.5), ylim = c(0.5, 10 + 0.5))
-
-points(1, 1, pch = 15)  # points are independent of the size of the plotting device!
-
-design.matrix <- expand.grid(1:ncols, 10:(1 + ncols - nrows))  # create a matrix for positions.
-design.matrix <- design.matrix[1:pop, ]  # truncate the matrix to population size.
 
 ## -----------------------------------------------
 ## (+) ToDo:
