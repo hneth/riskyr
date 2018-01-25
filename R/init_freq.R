268818d400b307832a85687f465e8f6b325b6a6f
hneth <hansneth@gmail.com>
2018-01-25 19:08:16 +0100

    added full set of 9 frequencies (+ documentation)

--- R/init_freq.R
+++ R/init_freq.R
@@ -106,7 +106,7 @@
 #'
 #'
 #' @references
-#' Consult \href{https://en.wikipedia.org/wiki/Statistical_population}{Wikipedia} for additional information.
+#' Consult \href{https://en.wikipedia.org/wiki/Statistical_population}{Wikipedia: Statistical population} for additional information.
 #'
 #' @family frequencies
 #'
@@ -350,16 +350,348 @@ cr <- 0  # default correct rejections (TN)
 ## -----------------------------------------------
 ## (B) Define and initialize COMBINED frequencies:
 ## -----------------------------------------------
-
-# +++ here now +++
-
-## -----------------------------------------------
-## (a) cond.true cond.false
+## (a) cond.true vs. cond.false
 ## -----------------------------------------------
 
+## (5) cond.true
+
+#' Number of individuals for which the condition is true.
+#'
+#' \code{cond.true} is a frequency that describes the
+#' number of individuals in the current population \code{\link{N}}
+#' for which the condition is \code{TRUE} (i.e., actually true cases).
+#'
+#' Key relationships:
+#'
+#' \enumerate{
+#'
+#' \item to probabilities:
+#' The frequency of \code{cond.true} individuals depends on the population size \code{\link{N}} and
+#' the condition's prevalence \code{\link{prev}} and is split further into two subsets of
+#' \code{\link{hi}} by the sensitivity \code{\link{sens}} and
+#' \code{\link{mi}} by the miss rate \code{\link{mirt}}.
+#'
+#' Perspectives:
+#'
+#' \enumerate{
+#'   \item by condition:
+#'
+#'   The frequency \code{\link{cond.true}} is determined by the population size \code{\link{N}} times the prevalence \code{\link{prev}}:
+#'
+#'   \code{ \link{cond.true} = \link{N} x \link{prev}}
+#'
+#'   \item by decision:
+#'
+#'   a. The frequency \code{\link{hi}} is determined by \code{\link{cond.true}} times the sensitivity \code{\link{sens}}
+#'   (aka. hit rate \code{\link{HR}}):
+#'
+#'   \code{ \link{hi} = \link{cond.true} x \link{sens}}
+#'
+#'
+#'   b. The frequency \code{\link{mi}} is determined by \code{\link{cond.true}} times the miss rate \code{\link{mirt} = (1 - \link{sens})}:
+#'
+#'   \code{ \link{mi} = \link{cond.true} x \link{mirt}  =  \link{cond.true} x (1 - \link{sens})}
+#'
+#' }
+#'
+#'
+#'   \item to other frequencies:
+#'   In a population of size \code{\link{N}} the following relationships hold:
+#'
+#'   \itemize{
+#'
+#'     \item \code{\link{N} = \link{cond.true} + \link{cond.false}} (by condition)
+#'
+#'     \item \code{\link{N} = \link{dec.pos} + \link{dec.neg}} (by decision)
+#'
+#'     \item \code{\link{N} = \link{hi} + \link{mi} + \link{fa} + \link{cr}} (by condition x decision)
+#'   }
+#'
+#' }
+#'
+#' Current frequency information is computed by
+#' \code{\link{comp_freq}} and contained in a list
+#' \code{\link{freq}}.
+#'
+#'
+#' @references
+#' Consult \href{https://en.wikipedia.org/wiki/Confusion_matrix}{Wikipedia: Confusion matrix} for additional information.
+#'
+#' @family frequencies
+#'
+#' @seealso
+#' \code{\link{is_freq}} verifies frequencies;
+#' \code{\link{num}} contains basic numeric parameters;
+#' \code{\link{init_num}} initializes basic numeric parameters;
+#' \code{\link{freq}} contains current frequency information;
+#' \code{\link{comp_freq}} computes current frequency information;
+#' \code{\link{prob}} contains current probability information;
+#' \code{\link{comp_prob}} computes current probability information.
+#'
+#' @examples
+#' cond.true <- 1000 * .10   # => sets cond.true to 10% of 1000 = 100 cases.
+#' is_freq(cond.true)        # => TRUE
+#' is_prob(cond.true)        # => FALSE, as cond.true is no probability (but prev and sens are)
+
+cond.true <- 0  # default frequency of true cases
+
+
 ## -----------------------------------------------
-## (b) dec.pos dec.neg
+## (6) cond.false
+
+#' Number of individuals for which the condition is false.
+#'
+#' \code{cond.false} is a frequency that describes the
+#' number of individuals in the current population \code{\link{N}}
+#' for which the condition is \code{FALSE} (i.e., actually false cases).
+#'
+#' Key relationships:
+#'
+#' \enumerate{
+#'
+#' \item to probabilities:
+#' The frequency of \code{cond.false} individuals depends on the population size \code{\link{N}} and
+#' the complement of the condition's prevalence \code{1 - \link{prev}} and is split further into two subsets of
+#' \code{\link{fa}} by the false alarm rate \code{\link{fart}} and
+#' \code{\link{cr}} by the specificity \code{\link{spec}}.
+#'
+#' Perspectives:
+#'
+#' \enumerate{
+#'   \item by condition:
+#'
+#'   The frequency \code{\link{cond.false}} is determined by the population size \code{\link{N}} times the complement of the prevalence \code{(1 - \link{prev})}:
+#'
+#'   \code{\link{cond.false}= \link{N} x (1 - \link{prev})}
+#'
+#'   \item by decision:
+#'
+#'   a. The frequency \code{\link{fa}} is determined by \code{\link{cond.false}} times the false alarm rate \code{\link{fart} = (1 - \link{spec})}
+#'   (aka. \code{\link{FPR}}):
+#'
+#'   \code{\link{fa} = \link{cond.false} x \link{fart} = \link{cond.false} x (1 - \link{spec}) }
+#'
+#'
+#'   b. The frequency \code{\link{cr}} is determined by \code{\link{cond.false}} times the specificity \code{\link{spec} = (1 - \link{fart})}:
+#'
+#'   \code{\link{cr}  =  \link{cond.false} x \link{spec}  =  \link{cond.false} x (1 - \link{fart}) }
+#'
+#' }
+#'
+#'
+#'   \item to other frequencies:
+#'   In a population of size \code{\link{N}} the following relationships hold:
+#'
+#'   \itemize{
+#'
+#'     \item \code{\link{N} = \link{cond.true} + \link{cond.false}} (by condition)
+#'
+#'     \item \code{\link{N} = \link{dec.pos} + \link{dec.neg}} (by decision)
+#'
+#'     \item \code{\link{N} = \link{hi} + \link{mi} + \link{fa} + \link{cr}} (by condition x decision)
+#'   }
+#'
+#' }
+#'
+#' Current frequency information is computed by
+#' \code{\link{comp_freq}} and contained in a list
+#' \code{\link{freq}}.
+#'
+#'
+#' @references
+#' Consult \href{https://en.wikipedia.org/wiki/Confusion_matrix}{Wikipedia: Confusion matrix} for additional information.
+#'
+#' @family frequencies
+#'
+#' @seealso
+#' \code{\link{is_freq}} verifies frequencies;
+#' \code{\link{num}} contains basic numeric parameters;
+#' \code{\link{init_num}} initializes basic numeric parameters;
+#' \code{\link{freq}} contains current frequency information;
+#' \code{\link{comp_freq}} computes current frequency information;
+#' \code{\link{prob}} contains current probability information;
+#' \code{\link{comp_prob}} computes current probability information.
+#'
+#' @examples
+#' cond.false <- 1000 * .90  # => sets cond.false to 90% of 1000 = 900 cases.
+#' is_freq(cond.false)       # => TRUE
+#' is_prob(cond.false)       # => FALSE, as cond.false is no probability [but (1 - prev) and spec are]
+
+cond.false <- 0  # default frequency of false cases
+
+
 ## -----------------------------------------------
+## (b) dec.pos vs. dec.neg
+## -----------------------------------------------
+
+## (7) dec.pos
+
+#' Number of individuals for which the decision is positive.
+#'
+#' \code{dec.pos} is a frequency that describes the
+#' number of individuals in the current population \code{\link{N}}
+#' for which the decision is positive (i.e., called or predicted cases).
+#'
+#' Key relationships:
+#'
+#' \enumerate{
+#'
+#' \item to probabilities:
+#' The frequency of \code{dec.pos} individuals depends on the population size \code{\link{N}} and
+#' the decision's proportion of positive decisions \code{\link{ppod}} and is split further into two subsets of
+#' \code{\link{hi}} by the positive predictive value \code{\link{PPV}} and
+#' \code{\link{fa}} by the false detection rate \code{\link{FDR} = 1 - \link{PPV}}.
+#'
+#' Perspectives:
+#'
+#' \enumerate{
+#'   \item by condition:
+#'
+#'   The frequency \code{\link{dec.pos}} is determined by the population size \code{\link{N}} times
+#'   the proportion of positive decisions \code{\link{ppod}}:
+#'
+#'   \code{\link{dec.pos}  =  \link{N} x \link{ppod}}
+#'
+#'   \item by decision:
+#'
+#'   a. The frequency \code{\link{hi}} is determined by \code{\link{dec.pos}} times the positive predictive value \code{\link{PPV}}
+#'   (aka. \code{\link{precision}}):
+#'
+#'   \code{\link{hi}  =  \link{dec.pos} x \link{PPV}}
+#'
+#'
+#'   b. The frequency \code{\link{fa}} is determined by \code{\link{dec.pos}} times the false detection rate \code{\link{FDR} = (1 - \link{PPV})}:
+#'
+#'   \code{\link{fa}  =  \link{dec.pos} x \link{FDR}  =  \link{dec.pos} x (1 - \link{PPV})}
+#'
+#' }
+#'
+#'
+#'   \item to other frequencies:
+#'   In a population of size \code{\link{N}} the following relationships hold:
+#'
+#'   \itemize{
+#'
+#'     \item \code{\link{N}  =  \link{cond.true} + \link{cond.false}} (by condition)
+#'
+#'     \item \code{\link{N}  =  \link{dec.pos} + \link{dec.neg}} (by decision)
+#'
+#'     \item \code{\link{N}  =  \link{hi} + \link{mi} + \link{fa} + \link{cr}} (by condition x decision)
+#'   }
+#'
+#' }
+#'
+#' Current frequency information is computed by
+#' \code{\link{comp_freq}} and contained in a list
+#' \code{\link{freq}}.
+#'
+#'
+#' @references
+#' Consult \href{https://en.wikipedia.org/wiki/Confusion_matrix}{Wikipedia: Confusion matrix} for additional information.
+#'
+#' @family frequencies
+#'
+#' @seealso
+#' \code{\link{is_freq}} verifies frequencies;
+#' \code{\link{num}} contains basic numeric parameters;
+#' \code{\link{init_num}} initializes basic numeric parameters;
+#' \code{\link{freq}} contains current frequency information;
+#' \code{\link{comp_freq}} computes current frequency information;
+#' \code{\link{prob}} contains current probability information;
+#' \code{\link{comp_prob}} computes current probability information.
+#'
+#' @examples
+#' dec.pos <- 1000 * .33   # => sets dec.pos to 33% of 1000 = 330 cases.
+#' is_freq(dec.pos)        # => TRUE
+#' is_prob(dec.pos)        # => FALSE, as dec.pos is no probability (but ppod and PPV are)
+
+dec.pos <- 0  # default frequency of positive decisions
+
+
+## -----------------------------------------------
+
+## (8) dec.neg
+
+#' Number of individuals for which the decision is negative.
+#'
+#' \code{dec.neg} is a frequency that describes the
+#' number of individuals in the current population \code{\link{N}}
+#' for which the decision is negative (i.e., cases not called or not predicted).
+#'
+#' Key relationships:
+#'
+#' \enumerate{
+#'
+#' \item to probabilities:
+#' The frequency of \code{dec.neg} individuals depends on the population size \code{\link{N}} and
+#' the decision's proportion of negative decisions \code{(1 - \link{ppod})} and is split further into two subsets of
+#' \code{\link{cr}} by the negative predictive value \code{\link{NPV}} and
+#' \code{\link{mi}} by the false omission rate \code{\link{FOR} = 1 - \link{NPV}}.
+#'
+#' Perspectives:
+#'
+#' \enumerate{
+#'   \item by condition:
+#'
+#'   The frequency \code{\link{dec.neg}} is determined by the population size \code{\link{N}} times
+#'   the proportion of negative decisions \code{(1 - \link{ppod})}:
+#'
+#'   \code{\link{dec.neg}  =  \link{N} x (1 - \link{ppod})}
+#'
+#'   \item by decision:
+#'
+#'   a. The frequency \code{\link{cr}} is determined by \code{\link{dec.neg}} times the negative predictive value \code{\link{NPV}}:
+#'
+#'   \code{\link{cr}  =  \link{dec.neg} x \link{NPV}}
+#'
+#'
+#'   b. The frequency \code{\link{mi}} is determined by \code{\link{dec.neg}} times the false omission rate \code{\link{FOR} = (1 - \link{NPV})}:
+#'
+#'   \code{\link{mi}  =  \link{dec.neg} x \link{FOR}  =  \link{dec.neg} x (1 - \link{NPV})}
+#'
+#' }
+#'
+#'
+#'   \item to other frequencies:
+#'   In a population of size \code{\link{N}} the following relationships hold:
+#'
+#'   \itemize{
+#'
+#'     \item \code{\link{N}  =  \link{cond.true} + \link{cond.false}} (by condition)
+#'
+#'     \item \code{\link{N}  =  \link{dec.pos} + \link{dec.neg}} (by decision)
+#'
+#'     \item \code{\link{N}  =  \link{hi} + \link{mi} + \link{fa} + \link{cr}} (by condition x decision)
+#'   }
+#'
+#' }
+#'
+#' Current frequency information is computed by
+#' \code{\link{comp_freq}} and contained in a list
+#' \code{\link{freq}}.
+#'
+#'
+#' @references
+#' Consult \href{https://en.wikipedia.org/wiki/Confusion_matrix}{Wikipedia: Confusion matrix} for additional information.
+#'
+#' @family frequencies
+#'
+#' @seealso
+#' \code{\link{is_freq}} verifies frequencies;
+#' \code{\link{num}} contains basic numeric parameters;
+#' \code{\link{init_num}} initializes basic numeric parameters;
+#' \code{\link{freq}} contains current frequency information;
+#' \code{\link{comp_freq}} computes current frequency information;
+#' \code{\link{prob}} contains current probability information;
+#' \code{\link{comp_prob}} computes current probability information.
+#'
+#' @examples
+#' dec.neg <- 1000 * .67   # => sets dec.neg to 67% of 1000 = 670 cases.
+#' is_freq(dec.neg)        # => TRUE
+#' is_prob(dec.neg)        # => FALSE, as dec.neg is no probability (but ppod, NPV and FOR are)
+
+dec.neg <- 0  # default frequency of negative decisions
+
 
 ## -----------------------------------------------
 ## (+) ToDo:
