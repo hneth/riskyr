017dedb989d932b4c2cd2fd2ba7c3432ba583811
hneth <hansneth@gmail.com>
2018-01-30 18:54:35 +0100

    compute plot_mosaic from probabilities (and only use popu when no valid set is provided)

--- R/plot_mosaic.R
+++ R/plot_mosaic.R
@@ -1,5 +1,5 @@
 ## plot_mosaic.R | riskyR
-## 2018 01 25
+## 2018 01 30
 ## -----------------------------------------------
 ## Plot mosaicplot that expresses freq as area
 ## (size and proportion)
@@ -7,16 +7,14 @@
 ## -----------------------------------------------
 ## Dependencies:
 
-# library("vcd") # moved to "Imports:" in in DESCRIPTION!
+# library("vcd")   # moved to "Imports:" in in DESCRIPTION!
 # library("grid")
 
 ## -----------------------------------------------
 ## plot_mosaic: Plot mosaic plot (with "vcd" and "grid")
 ## using only necessary arguments with good defaults:
-
-## Assuming that df of popu is known [see comp_popu()]
-
 ## -----------------------------------------------
+
 
 #' Plot a mosaic plot of population frequencies.
 #'
@@ -25,12 +23,54 @@
 #' population \code{\link{popu}} as relatives sizes of
 #' rectangular areas.
 #'
-#' Corresponding rectangles can be visualized by
-#' opting for vertical rectangles (by selecting
-#' the option \code{box = "vr"}) in \code{\link{plot_tree}}.
+#' If a sufficient and valid set of 3 essential probabilities
+#' (\code{\link{prev}}, and
+#' \code{\link{sens}} or its complement \code{\link{mirt}}, and
+#' \code{\link{spec}} or its complement \code{\link{fart}})
+#' is provided, new frequency information \code{\link{freq}}
+#' and a new population table \code{\link{popu}}
+#' are computed from scratch.  Otherwise, the existing
+#' population \code{\link{popu}} is shown.
+#'
+#' Rectangles corresponding to the areas of the mosaic plot
+#' can be visualized byopting for vertical rectangles (by selecting
+#' the option \code{box = "vr"}) in \code{\link{plot_tree}}
+#' and \code{\link{plot_fnet}}.
 #'
 #' \code{plot_mosaic} requires and uses the R packages "vcd" and
 #' "grid" (\code{library("vcd", "grid")}).
+#'
+#'
+#' @param prev The condition's prevalence \code{\link{prev}}
+#' (i.e., the probability of condition being \code{TRUE}).
+#'
+#' @param sens The decision's sensitivity \code{\link{sens}}
+#' (i.e., the conditional probability of a positive decision
+#' provided that the condition is \code{TRUE}).
+#' \code{sens} is optional when its complement \code{mirt} is provided.
+#'
+#' @param mirt The decision's miss rate \code{\link{mirt}}
+#' (i.e., the conditional probability of a negative decision
+#' provided that the condition is \code{TRUE}).
+#' \code{mirt} is optional when its complement \code{sens} is provided.
+#'
+#' @param spec The decision's specificity value \code{\link{spec}}
+#' (i.e., the conditional probability
+#' of a negative decision provided that the condition is \code{FALSE}).
+#' \code{spec} is optional when its complement \code{fart} is provided.
+#'
+#' @param fart The decision's false alarm rate \code{\link{fart}}
+#' (i.e., the conditional probability
+#' of a positive decision provided that the condition is \code{FALSE}).
+#' \code{fart} is optional when its complement \code{spec} is provided.
+#'
+#'
+#' @param N The number of individuals in the population.
+#' (This value is not represented in the plot,
+#' but used when new frequency information \code{\link{freq}}
+#' and a new population table \code{\link{popu}}
+#' are computed from scratch from current probabilities.)
+#'
 #'
 #' @examples
 #' plot_mosaic()
@@ -44,32 +84,77 @@
 #' \code{\link{num}} for basic numeric parameters;
 #' \code{\link{txt}} for current text settings;
 #' \code{\link{pal}} for current color settings
+#'
+#' @export
 
-plot_mosaic <- function(pop = popu,
+plot_mosaic <- function(prev = num$prev,             # probabilities
+                        sens = num$sens, mirt = NA,
+                        spec = num$spec, fart = NA,
+                        N = num$N,                   # not needed in Mosaic plot (but used in comp_freq below)
+                        ## Options: ##
                         vsplit = TRUE, # option: toggle vertical vs. horizontal split in plot
                         title.lbl = txt$scen.lbl,
-                        col.sdt = pal[4:7]
-                        ) {
+                        col.sdt = c(pal["hi"], pal["mi"], pal["fa"], pal["cr"])
+) {
 
-  ## Get probabilities from global numeric parameters (num):
-  prev <- num$prev
-  sens <- num$sens
-  spec <- num$spec
+  # ## (0) Get probabilities from global numeric parameters (num):
+  # prev <- num$prev
+  # sens <- num$sens
+  # spec <- num$spec
 
-  ## Define plot area:
+
+  ## (1) Compute or use current popu:
+
+  ## (A) If a valid set of probabilities was provided:
+  if (is_valid_prob_set(prev = prev, sens = sens, mirt = mirt, spec = spec, fart = fart, tol = .01)) {
+
+    ## (a) Compute the complete quintet of probabilities:
+    prob_quintet <- comp_complete_prob_set(prev, sens, mirt, spec, fart)
+    sens <- prob_quintet[2] # gets sens (if not provided)
+    mirt <- prob_quintet[3] # gets mirt (if not provided)
+    spec <- prob_quintet[4] # gets spec (if not provided)
+    fart <- prob_quintet[5] # gets fart (if not provided)
+
+    ## (b) Compute cur.freq and popu based on current parameters (N and probabilities):
+    cur.freq <- comp_freq(prev = prev, sens = sens, spec = spec, N = N, round = TRUE)  # compute cur.freq (with round = TRUE)
+
+    ## Assign (only needed) elements of cur.freq:
+    n.hi  <- cur.freq$hi
+    n.mi  <- cur.freq$mi
+    n.fa  <- cur.freq$fa
+    n.cr  <- cur.freq$cr
+
+    ## (c) Compute cur.popu from computed frequencies:
+    cur.popu <- comp_popu(hi = n.hi, mi = n.mi, fa = n.fa, cr = n.cr)  # compute cur.popu (from 4 essential frequencies)
+
+    # warning("Generated new population (cur.popu) to draw mosaic plot...")
+
+
+  } else {  # (B) NO valid set of probabilities was provided:
+
+    ## Use the current popu:
+    cur.popu <- popu
+
+    # warning("Using existing population (popu) to draw mosaic plot...")
+
+  } # if (is_valid_prob_set...)
+
+
+
+  ## (2) Define plot area:
   # plot(0, type = 'n')
 
-  ## Text labels:
+  ## (3) Text labels:
   if (nchar(title.lbl) > 0) { title.lbl <- paste0(title.lbl, ":\n") }  # put on top (in separate line)
   cur.title.lbl <- paste0(title.lbl, "Mosaic plot") # , "(N = ", N, ")")
   cur.par.lbl <-  paste0("(", "prev = ", as_pc(prev), "%, ", "sens = ", as_pc(sens), "%, ", "spec = ", as_pc(spec), "%)")
 
-  ## Mosaic plot:
+  ## (4) Mosaic plot:
 
   if (vsplit) {
 
     ## (a) vertical split:
-    vcd::mosaic(Decision ~ Truth, data = pop,
+    vcd::mosaic(Decision ~ Truth, data = cur.popu,
                 shade = TRUE, colorize = TRUE,
                 split_vertical = TRUE,
                 gp = grid::gpar(fill = matrix(data = col.sdt, nrow = 2, ncol = 2, byrow = TRUE)),
@@ -77,13 +162,13 @@ plot_mosaic <- function(pop = popu,
                 sub_gp = grid::gpar(fontsize = 10, fontface = 1, adj = 1),
                 main = paste0(cur.title.lbl), #, "\n", cur.par.lbl),
                 sub = paste0(cur.par.lbl)
-                )
+    )
   }
 
   else {
 
     ## (b) horizontal split:
-    vcd::mosaic(Truth ~ Decision, data = pop,
+    vcd::mosaic(Truth ~ Decision, data = cur.popu,
                 shade = TRUE, colorize = TRUE,
                 split_vertical = FALSE,
                 gp = grid::gpar(fill = matrix(data = col.sdt, nrow = 2, ncol = 2, byrow = FALSE)),
@@ -91,7 +176,7 @@ plot_mosaic <- function(pop = popu,
                 sub_gp = grid::gpar(fontsize = 10, fontface = 1),
                 main = paste0(cur.title.lbl), #, "\n", cur.par.lbl),
                 sub = paste0(cur.par.lbl)
-                )
+    )
 
   }
 
