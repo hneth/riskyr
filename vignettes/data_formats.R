ef5eb1495463125b52f63f5572e379d11a8879f0
hneth <hansneth@gmail.com>
2018-02-04 22:27:28 +0100

    minor changes

---
+++ vignettes/data_formats.R
@@ -0,0 +1,18 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----plot_fnet, fig.width = 7.1, fig.height = 7, fig.show = 'asis'-------
+plot_fnet(prev = .01, sens = .80, spec = NA, fart = .096, N = 1000, 
+          area = "no", 
+          title.lbl = "Mammography screening")
+
+## ---- fig.show='hold'----------------------------------------------------
+plot(1:10)
+plot(10:1)
+
+## ---- echo=FALSE, results='asis'-----------------------------------------
+knitr::kable(head(mtcars, 10))
+
