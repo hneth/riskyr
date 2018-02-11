69fe13daa051da57ac6111b3feaf9eb0e78bb219
hneth <hansneth@gmail.com>
2018-02-11 23:15:14 +0100

    add vignette info

--- vignettes/materials.R
+++ vignettes/materials.R
@@ -6,6 +6,12 @@ knitr::opts_chunk$set(
 
 library("riskyr")  # load the "riskyr" package
 
+## ----plot_curve, fig.width = 7.2, fig.height = 5, fig.show = 'asis', fig.cap = "A plot that shows PPV and NPV as a function of prevalence (for given values of sensitivity and specificity)."----
+library("riskyr")  # load the "riskyr" package
+
+plot_curve(prev = .01, sens = .80, spec = (1 - .096), 
+           log.scale = TRUE, title.lbl = "Mammography screening")
+
 ## ---- fig.show='hold'----------------------------------------------------
 plot(1:10)
 plot(10:1)
