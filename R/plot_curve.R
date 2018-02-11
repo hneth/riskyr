e8bce5ead9a577fa05d8b51d5172f10e7623ebf1
hneth <hansneth@gmail.com>
2018-02-11 16:55:50 +0100

    generalized version of plot_PV function

--- R/plot_curve.R
+++ R/plot_curve.R
@@ -1,8 +1,8 @@
 ## plot_curve.R | riskyr
 ## 2018 02 11
 ## -----------------------------------------------
-## plot_curve: A generalization of plot_PV that
-## plots different DVs (e.g., PPV, NPV, acc curves)
+## plot_curve: A generalization of plot_PV
+## that plots different DVs (e.g., PPV, NPV, acc curves)
 ## as functions of prevalence.
 
 ## -----------------------------------------------
@@ -93,23 +93,32 @@
 #' Default: \code{col.npv = pal["npv"]}.
 #'
 #' @examples
-#' plot_PV()
-#' plot_PV(prev = .0001, log.scale = TRUE)
-#' plot_PV(title.lbl = "My favorite scenario", show.PVprev = FALSE)
-#' plot_PV(title.lbl = "", col.ppv = "firebrick3", col.npv = "steelblue3")
+#' #'
+#' # ways to work:
+#' plot_curve()  # => default curves (prev, PPV, NPV)
+#' plot_curve(show.points = FALSE)  # => default without points
+#' plot_curve(what = c("PPV", "NPV"), show.points = TRUE)  # => prev not shown.
+#'
+#' # linear vs. log scale:
+#' plot_curve(prev = .01, sens = .9, spec = .8)                     # => linear scale
+#' plot_curve(prev = .01, sens = .9, spec = .8, log.scale = TRUE)   # => log scale
+#'
+#' plot_curve(prev = .0001, sens = .7, spec = .6)                   # => linear scale
+#' plot_curve(prev = .0001, sens = .7, spec = .6, log.scale = TRUE) # => log scale
+#' #'
 #'
 #'
 #' @family visualization functions
 #'
 #'
 #' @seealso
-#' \code{\link{comp_popu}} computes the current population;
-#' \code{\link{popu}} contains the current population;
+#' \code{\link{comp_prob}} computes current probability information;
+#' \code{\link{prob}} contains current probability information;
 #' \code{\link{comp_freq}} computes current frequency information;
 #' \code{\link{freq}} contains current frequency information;
 #' \code{\link{num}} for basic numeric parameters;
 #' \code{\link{txt}} for current text settings;
-#' \code{\link{pal}} for current color settings
+#' \code{\link{pal}} for current color settings.
 #'
 #' @export
 
@@ -117,20 +126,17 @@
 plot_curve <- function(prev = num$prev,             # probabilities (3 essential, 2 optional)
                        sens = num$sens, mirt = NA,
                        spec = num$spec, fart = NA,
-                       ## DVs: ##
+                       ## DVs:
                        what = c("prev", "PPV", "NPV"),  # what curves?  Options: "acc", "ppod"
-                       show.points = TRUE, # show points at current prev?
-                       what.col = pal,     # colors for what.
-                       ## Options: ##
-                       show.PVprev = TRUE,          # user options [adjustable by inputs]
-                       show.PVpoints = TRUE,
-                       log.scale = FALSE,
-                       title.lbl = txt$scen.lbl,    # custom labels and colors
-                       col.ppv = pal["ppv"],
-                       col.npv = pal["npv"]
-) {
+                       what.col = pal,                  # colors for what.
+                       ## Options:
+                       show.points = TRUE,  # show points at current prev?
+                       log.scale = FALSE,   # x-axis on log scale?
+                       title.lbl = txt$scen.lbl
+                       ) {
 
-  ## (0) Compute or collect current probabilities:
+  ## (0) Compute or collect current probabilities: ----------
+
   if (is_valid_prob_set(prev = prev, sens = sens, mirt = mirt, spec = spec, fart = fart, tol = .01)) {
 
     ## (1) A provided set of probabilities is valid:
@@ -159,40 +165,11 @@ plot_curve <- function(prev = num$prev,             # probabilities (3 essential
 
   }
 
+  ## (1) Set some (currently fixed) parameters: ----------
 
-  ## Labels:
-  prev.lbl <- paste0("prev = ", as_pc(prev), "%")
-  cur.PPV.lbl <- paste0("PPV = ", as_pc(cur.PPV), "%")
-  cur.NPV.lbl <- paste0("NPV = ", as_pc(cur.NPV), "%")
-  cur.PV.lbl <- paste0(cur.PPV.lbl, ", ", cur.NPV.lbl)
-  cur.sens.spec.lbl <- paste0("(sens = ", as_pc(sens), "%, spec = ", as_pc(spec), "%)")
-  cur.par.lbl <-  paste0("(", "prev = ", as_pc(prev), "%, ", "sens = ", as_pc(sens), "%, ", "spec = ", as_pc(spec), "%)")
-  if (nchar(title.lbl) > 0) { title.lbl <- paste0(title.lbl, ":\n") }  # put on top (in separate line)
-  cur.title.lbl <- paste0(title.lbl, "Positive and Negative Predictive Values") #, "\n", cur.sens.spec.lbl)
-
-  if (log.scale) {
-    x.ax.lbl <- "Prevalence (on logarithmic scale)"
-  } else {
-    x.ax.lbl <- "Prevalence"
-  }
-
-  if (log.scale) {
-    x.seq <- c(10^-5, 10^-4, 10^-3, 10^-2, .10, .25, .50, 1)
-    x.lbl <- paste0(as_pc(x.seq, n.digits = 5), "%")  # log percentages (rounded to 5 decimals)
-  } else {
-    x.seq <- seq(0, 1, by = .1)
-    x.lbl <- paste0(as_pc(x.seq), "%")  # linear percentages
-  }
-
-  y.seq <- seq(0, 1, by = .1)
-  y.lbl <- paste0(as_pc(y.seq), "%")  # linear percentages
-
-
-  ## Parameters:
-  col.prev <- grey(.50, alpha = .99)  # prevalence # WAS: col.green.2
   col.axes <- grey(.10, alpha = .99)  # axes
   col.bord <- grey(.10, alpha = .50)  # borders (e.g., of points)
-  cex.lbl <- .8 # size of text labels
+  cex.lbl <- .8                       # size of text labels
 
   if (log.scale) { x.min <- 10^-6 } else { x.min <- 0 }  # different x.min values for different scales
   if (log.scale) { h.shift <- prev * 2 } else { h.shift <- .080 }
@@ -200,19 +177,36 @@ plot_curve <- function(prev = num$prev,             # probabilities (3 essential
   low.PV  <- .200  # threshold value for judging PPV or NPV to be low
   v.raise <- .700  # vertical raise of y-prev when PPV or NPV < low.PV
 
+  legend.lbls <- NULL  # initialize vector
+  legend.cols <- NULL
+  legend.ltys <- NULL
 
-  ## Initialize plot:
+
+  ## (2) Initialize plotting area: : ----------
+
+  ## (a) Define steps and labels of x- and y-axes:
   if (log.scale) {
-
-    plot(0, xlim = c(x.min, 1), ylim = c(0, 1), axes = FALSE, log = "x", ylab = "Probability", xlab = x.ax.lbl, type = "n")
-
+    x.seq <- c(10^-5, 10^-4, 10^-3, 10^-2, .10, .25, .50, 1)  # log steps
+    x.lbl <- paste0(as_pc(x.seq, n.digits = 5), "%")          # log percentages (rounded to 5 decimals)
+    x.ax.lbl <- "Prevalence (on logarithmic scale)"           # log x-axis label
   } else {
-
-    plot(0, xlim = c(x.min, 1), ylim = c(0, 1), axes = FALSE, ylab = "Probability", xlab = x.ax.lbl, type = "n")
-
+    x.seq <- seq(0, 1, by = .1)         # linear steps
+    x.lbl <- paste0(as_pc(x.seq), "%")  # linear percentages
+    x.ax.lbl <- "Prevalence"            # linear x-axis label
   }
 
-  ## Axes (on 4 sides):
+  y.seq <- seq(0, 1, by = .1)         # linear steps
+  y.lbl <- paste0(as_pc(y.seq), "%")  # linear percentages
+  y.ax.lbl <- "Probability"           # y-axis label
+
+  ## (b) Initialize plot:
+  if (log.scale) {
+    plot(0, xlim = c(x.min, 1), ylim = c(0, 1), axes = FALSE, log = "x", ylab = "Probability", xlab = x.ax.lbl, type = "n")
+  } else {
+    plot(0, xlim = c(x.min, 1), ylim = c(0, 1), axes = FALSE, ylab = "Probability", xlab = x.ax.lbl, type = "n")
+  }
+
+  ## (c) Axes (on 4 sides):
   axis(side = 1, at = x.seq, labels = x.lbl, cex.axis = cex.lbl, las = 1,
        pos = 0, tck = -.02, col.axis = col.axes, col.ticks = col.axes) # x at bottom
   axis(side = 1, at = x.seq, labels = FALSE, cex.axis = cex.lbl, las = 1,
@@ -222,127 +216,179 @@ plot_curve <- function(prev = num$prev,             # probabilities (3 essential
   axis(side = 4, at = y.seq, labels = y.lbl, cex.axis = cex.lbl, las = 1,
        pos = 1, tck = -.02, col.axis = col.axes, col.ticks = col.axes) # y at right
 
-  ## Grid:
+  ## (d) Grid:
   grid(col = grey(.8, .8))
 
 
-  ## Plot stuff: ##
+  ## (3) Plot elements of what: ----------
 
-  ## Curves:
-  curve(expr = comp_PPV(x, sens, spec), from = x.min, to = 1, add = TRUE, lty = 1, lwd = 2, col = col.ppv) # PPV curve
-  curve(expr = comp_NPV(x, sens, spec), from = x.min, to = 1, add = TRUE, lty = 1, lwd = 2, col = col.npv) # NPV curve
+  ## (a) prev:
+  if ("prev" %in% what) {
 
-  ## Lines:
-  if (show.PVprev){
-    abline(v = prev, lty = 2, lwd = 1, col = col.prev) # prev as vline
-  }
-  # abline(h = PPV, lty = 3, lwd = 1, col = col.ppv) # PPV as hline
-  # abline(h = NPV, lty = 3, lwd = 1, col = col.npv) # NPV as hline
-  # segments(x0 = 0, y0 = PPV, x1 = prev, y1 = PPV, lty = 2, lwd = 1.5, col = col.ppv) # PPV to y-axes
-  # segments(x0 = 0, y0 = NPV, x1 = prev, y1 = NPV, lty = 2, lwd = 1.5, col = col.npv) # NPV to y-axes
+    ## 0. parameters:
+    lty.prev <- 2                            # prev line type
+    col.prev <- grey(.50, alpha = .99)       # prev color
+    legend.lbls <- c(legend.lbls, "prev")    # add prev label
+    legend.cols <- c(legend.cols, col.prev)  # add prev color
+    legend.ltys <- c(legend.ltys, lty.prev)  # add prev line type
 
-  ## Points:
-  ## prev:
-  if (show.PVprev){
-    if ((cur.NPV < low.PV) | (cur.PPV < low.PV)) { # y at v.raise:
-      points(x = prev, y = 0 + v.raise, pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.prev)
-    } else { # y at bottom (y = 0):
-      points(x = prev, y = 0, pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.prev)
-    }
-  }
+    ## 1. curve: prev as vline
+    abline(v = prev, lty = lty.prev, lwd = 1, col = col.prev)  # prev curve/line
 
-  if (show.PVpoints) {
-    ## PPV:
-    points(x = prev, y = cur.PPV, pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.ppv)
-    ## NPV:
-    points(x = prev, y = cur.NPV, pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.npv)
-  }
+    ## 2. point:
+    if (show.points) {
 
-  ## Labels:
-
-  ## prev label:
-  if (show.PVprev){
-    if ((cur.NPV < low.PV) | (cur.PPV < low.PV)) { # y at v.raise:
-      if ((prev < .50) | !(prev > 1 - h.shift)) {
-        text(x = prev + h.shift, y = 0 + v.raise,
-             labels = prev.lbl, col = col.prev, cex = cex.lbl) # on right
-      } else {
-        text(x = prev - h.shift, y = 0 + v.raise,
-             labels = prev.lbl, col = col.prev, cex = cex.lbl) # on left+
+      if ((cur.NPV < low.PV) | (cur.PPV < low.PV)) { # y-pos at v.raise:
+        points(x = prev, y = 0 + v.raise, pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.prev)  # prev point
+      } else { # y-pos at bottom (y = 0):
+        points(x = prev, y = 0,           pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.prev)  # prev point
       }
-    } else { # y at bottom (y = 0):
-      if ((prev < .50) | !(prev > 1 - h.shift)) {
-        text(x = prev + h.shift, y = 0 + v.shift,
-             labels = prev.lbl, col = col.prev, cex = cex.lbl) # on right
-      } else {
-        text(x = prev - h.shift, y = 0 + v.shift,
-             labels = prev.lbl, col = col.prev, cex = cex.lbl) # on left+
+
+      ## 3. label:
+      prev.lbl <- paste0("prev = ", as_pc(prev), "%")  # prev label
+
+      if ((cur.NPV < low.PV) | (cur.PPV < low.PV)) { # y at v.raise:
+        if ((prev < .50) | !(prev > 1 - h.shift)) {
+          text(x = prev + h.shift, y = 0 + v.raise,
+               labels = prev.lbl, col = col.prev, cex = cex.lbl) # on right
+        } else {
+          text(x = prev - h.shift, y = 0 + v.raise,
+               labels = prev.lbl, col = col.prev, cex = cex.lbl) # on left+
+        }
+      } else { # y at bottom (y = 0):
+        if ((prev < .50) | !(prev > 1 - h.shift)) {
+          text(x = prev + h.shift, y = 0 + v.shift,
+               labels = prev.lbl, col = col.prev, cex = cex.lbl) # on right
+        } else {
+          text(x = prev - h.shift, y = 0 + v.shift,
+               labels = prev.lbl, col = col.prev, cex = cex.lbl) # on left+
+        }
       }
-    }
-  }
 
-  if (show.PVpoints) {
-    ## PPV label:
-    if ((cur.PPV < .75 & !(prev > 1 - h.shift)) | (prev < h.shift)) {
-      text(x = prev + h.shift, y = cur.PPV + v.shift,
-           labels = cur.PPV.lbl, col = col.ppv, cex = cex.lbl) # on right
-    } else {
-      text(x = prev - h.shift, y = cur.PPV + v.shift,
-           labels = cur.PPV.lbl, col = col.ppv, cex = cex.lbl) # on left+
-    }
-    ## NPV label:
-    if (cur.NPV > .75 | (prev < h.shift)) {
-      text(x = prev + h.shift, y = cur.NPV + v.shift,
-           labels = cur.NPV.lbl, col = col.npv, cex = cex.lbl) # on right+
-    } else {
-      text(x = prev - h.shift, y = cur.NPV - v.shift,
-           labels = cur.NPV.lbl, col = col.npv, cex = cex.lbl) # on left-
-    }
-  }
+    } # if (show.points)...
 
-  ## Title:
+  } # if ("prev" %in% what)...
+
+
+  ## (b) PPV:
+  if ("PPV" %in% what || "ppv" %in% what) {
+
+    ## 0. parameters:
+    lty.ppv <- 1                            # PPV line type
+    col.ppv <- pal["ppv"]                   # PPV color
+    legend.lbls <- c(legend.lbls, "PPV")    # add PPV label
+    legend.cols <- c(legend.cols, col.ppv)  # add PPV color
+    legend.ltys <- c(legend.ltys, lty.ppv)  # add PPV line type
+
+    ## 1. curve: PPV as curve
+    curve(expr = comp_PPV(x, sens, spec), from = x.min, to = 1, add = TRUE, lty = lty.ppv, lwd = 2, col = col.ppv)  # PPV curve
+
+    ## 2. point:
+    if (show.points) {
+
+      points(x = prev, y = cur.PPV, pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.ppv)  # PPV point
+
+      ## 3. label:
+      PPV.lbl <- paste0("PPV = ", as_pc(cur.PPV), "%")  # PPV label
+
+      if ((cur.PPV < .75 & !(prev > 1 - h.shift)) || (prev < h.shift)) {
+        text(x = prev + h.shift, y = cur.PPV + v.shift,
+             labels = PPV.lbl, col = col.ppv, cex = cex.lbl) # on right
+      } else {
+        text(x = prev - h.shift, y = cur.PPV + v.shift,
+             labels = PPV.lbl, col = col.ppv, cex = cex.lbl) # on left+
+      }
+
+    } # if (show.points)...
+
+  } # if ("PPV" %in% what)...
+
+
+  ## (c) NPV:
+  if ("NPV" %in% what || "npv" %in% what) {
+
+    ## 0. parameters:
+    lty.npv <- 1                            # PPV line type
+    col.npv <- pal["npv"]                   # NPV color
+    legend.lbls <- c(legend.lbls, "NPV")    # add NPV label
+    legend.cols <- c(legend.cols, col.npv)  # add NPV color
+    legend.ltys <- c(legend.ltys, lty.npv)  # add NPV line type
+
+    ## 1. curve: NPV as curve
+    curve(expr = comp_NPV(x, sens, spec), from = x.min, to = 1, add = TRUE, lty = lty.npv, lwd = 2, col = col.npv)  # NPV curve
+
+    ## 2. point:
+    if (show.points) {
+
+      points(x = prev, y = cur.NPV, pch = 21, cex = 2, lwd = 1.5, col = col.bord, bg = col.npv)  # PPV point
+
+      ## 3. label:
+      NPV.lbl <- paste0("NPV = ", as_pc(cur.NPV), "%")  # NPV label
+
+      if (cur.NPV > .75 | (prev < h.shift)) {
+        text(x = prev + h.shift, y = cur.NPV + v.shift,
+             labels = NPV.lbl, col = col.npv, cex = cex.lbl) # on right+
+      } else {
+        text(x = prev - h.shift, y = cur.NPV - v.shift,
+             labels = NPV.lbl, col = col.npv, cex = cex.lbl) # on left-
+      }
+
+    } # if (show.points)...
+
+  } # if ("NPV" %in% what)...
+
+
+  ## (4) Title: ----------
+
+  if (nchar(title.lbl) > 0) { title.lbl <- paste0(title.lbl, ":\n") }  # put on top (in separate line)
+  cur.title.lbl <- paste0(title.lbl, "Curves as a Function of Prevalence") #, "\n", cur.sens.spec.lbl)
+
   title(cur.title.lbl, adj = 0.0, line = 1.0, font.main = 1) # (left, raised, normal font)
 
-  ## Margin text:
-  # if (show.PVpoints) {
-  #  mtext(paste0(cur.PV.lbl), side = 1, line = 2, adj = 1, col = grey(.11, .99), cex = .90, font = 1)
-  # }
-  # mtext(paste0(cur.par.lbl), side = 1, line = 3, adj = 1, col = grey(.11, .99), cex = .80)
-  mtext(paste0(cur.sens.spec.lbl), side = 1, line = 2, adj = 1, col = grey(.11, .99), cex = .90)
+
+  ## (5) Margin text: ----------
+
+  ## (a) by condition: 3 basic probabilities
+  cur.cond.lbl <- make_cond_lbl(prev, sens, spec)  # use utility function to format label
+  mtext(cur.cond.lbl, side = 1, line = 2, adj = 0, col = grey(.33, .99), cex = .85)  # print label
+
+  ## +++ here now +++
 
 
-  ## Legend:
+  ## (6) Legend: ----------
+
   # legend("bottom", legend = c("PPV", "NPV"),
   #       col = c(col.ppv, col.npv), lty = 1, lwd = 2, cex = 1, bty = "o", bg = "white")
-  add_legend("topright", legend = c("PPV", "NPV"), lwd = 2, col = c(col.ppv, col.npv),
-             horiz = FALSE, bty = 'n')
-
+  add_legend("topright",
+             legend = legend.lbls, lty = legend.ltys, lwd = 2, col = legend.cols,
+             cex = .90, horiz = FALSE, bty = 'n')
 
   ## Return what?
-  # return(pp)    # returns plot (as diagram object)
-  # return()      # returns nothing
-  # return("neat")  # returns "..."
+  # return(pp)     # returns plot
+  # return()       # returns nothing
+  # return("neat") # returns "..."
 
 }
 
 ## Check:
 {
-  # plot_PV() # default
-  # plot_PV(.01, sens, spec, show.PVprev = TRUE, show.PVpoints = TRUE)
-  # plot_PV(.45, sens, spec, show.PVprev = TRUE, show.PVpoints = TRUE)
-  # plot_PV(.55, sens, spec, show.PVprev = TRUE, show.PVpoints = TRUE)
-  # plot_PV(.99, sens, spec, show.PVprev = TRUE, show.PVpoints = TRUE,
-  #         col.ppv = "firebrick", col.npv = "steelblue3")
-  ## log scale:
-  # plot_PV(prev = .0001, sens = .95, spec = .66, log.scale = TRUE)
-  # plot_PV(prev = .0010, sens = .95, spec = .66, log.scale = TRUE)
-  # plot_PV(prev = .99, sens = .95, spec = .66, log.scale = TRUE)
+  # # ways to work:
+  # plot_curve()  # => default curves (prev, PPV, NPV)
+  # plot_curve(show.points = FALSE)  # => default without points
+  # plot_curve(what = c("PPV", "NPV"), show.points = TRUE)  # => prev not shown.
+  #
+  # # linear vs. log scale:
+  # plot_curve(prev = .01, sens = .9, spec = .8)                     # => linear scale
+  # plot_curve(prev = .01, sens = .9, spec = .8, log.scale = TRUE)   # => log scale
+  #
+  # plot_curve(prev = .0001, sens = .7, spec = .6)                   # => linear scale
+  # plot_curve(prev = .0001, sens = .7, spec = .6, log.scale = TRUE) # => log scale
 }
 
 ## -----------------------------------------------
 ## (+) ToDo:
 
-## - Add documentation.
+## - Add more options: ppod, accu, etc.
 ## - fine-tune positions of labels and legend (on linear vs. log scale)
 ## - pimp plot (titles, axes, grid, colors, transparency)
 
