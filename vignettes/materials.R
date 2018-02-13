22c250d2b5cd340947d90db80a5735db4793bed2
hneth <hansneth@gmail.com>
2018-02-13 13:51:38 +0100

    update vignette info

--- vignettes/materials.R
+++ vignettes/materials.R
@@ -6,22 +6,17 @@ knitr::opts_chunk$set(
 
 library("riskyr")  # load the "riskyr" package
 
-## ----plot_curve, fig.width = 7.2, fig.height = 5, fig.show = 'asis', fig.cap = "Curves that show PPV and NPV as a function of prevalence (for given values of sensitivity and specificity)."----
+## ----plot_curve, fig.width = 7.2, fig.height = 5, fig.show = 'asis', fig.cap = "Curves that show PPV and NPV as a function of an increased prevalence (for given values of sensitivity and specificity)."----
 library("riskyr")  # load the "riskyr" package
+high.prev <- .60   # assume increased prevalence due to BRCA1 mutation
 
-plot_curve(prev = .01, sens = .80, spec = (1 - .096), 
-           log.scale = TRUE, title.lbl = "Mammography screening")
+plot_curve(prev = high.prev, sens = .70, spec = (1 - .096), 
+           what = c("PPV", "NPV", "acc"), 
+           title.lbl = "Mammography screening (BRCA1 mutation)", cex.lbl = .8)
 
-## ----plot_plane, fig.width = 6, fig.height = 5, fig.show = 'asis', fig.cap = "Planes that show PPV, NPV, the proportion of positive predictions (ppod) and overall accuracy (acc) as a function of sensitivity and specificity (for given prevalence)."----
-plot_plane(prev = .01, sens = .80, spec = (1 - .096), what = "PPV",  title.lbl = "Mammography screening (A)")
-plot_plane(prev = .01, sens = .80, spec = (1 - .096), what = "NPV",  title.lbl = "Mammography screening (B)")
-plot_plane(prev = .01, sens = .80, spec = (1 - .096), what = "ppod", title.lbl = "Mammography screening (C)")
-plot_plane(prev = .01, sens = .80, spec = (1 - .096), what = "acc",  title.lbl = "Mammography screening (D)")
-
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
-
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ----plot_plane, fig.width = 6, fig.height = 4.5, fig.show = 'asis', fig.cap = "Planes that show PPV, NPV, the proportion of positive predictions (ppod) and overall accuracy (acc) as a function of sensitivity and specificity (for given prevalence)."----
+plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "PPV",  title.lbl = "A. Mammography (BRCA1)", cex.lbl = .8)
+plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "NPV",  title.lbl = "B. Mammography (BRCA1)", cex.lbl = .8)
+plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "ppod", title.lbl = "C. Mammography (BRCA1)", cex.lbl = .8)
+plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "acc",  title.lbl = "D. Mammography (BRCA1)", cex.lbl = .8)
 
