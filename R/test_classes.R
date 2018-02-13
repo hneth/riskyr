1c753f8d3ea0f83b9bc9777732ed388452adc3fd
hneth <hansneth@gmail.com>
2018-02-13 13:50:20 +0100

    update class info

--- R/test_classes.R
+++ R/test_classes.R
@@ -1,5 +1,5 @@
 ## test_classes.R | riskyr
-## 2018 02 12
+## 2018 02 13
 ## -----------------------------------------------
 ## Define riskyr class and corresponding methods:
 ## -----------------------------------------------
@@ -25,7 +25,7 @@ plot.riskyr <- function(obj,
                         plottype = "network",  # plottype parameter for type of plot.
                         # type = "array",  # type parameter for plot subtypes.
                         ...  # ellipsis for additional type parameters for the plotting functions.
-                             ) {
+) {
 
   ## Testing plottype argument:
   if (!plottype %in% c("network", "tree", "curve", "iconarray", "mosaicplot", "plane")) {
@@ -33,182 +33,187 @@ plot.riskyr <- function(obj,
   }
 
   # Recovering additional parameters:
-    arguments <- list(...)
-    arg.names <- names(arguments)
+  arguments <- list(...)
+  arg.names <- names(arguments)
 
-    # all:
-    if ("show.accu" %in% arg.names) show.accu <- arguments$show.accu else show.accu = TRUE
-    if ("area" %in% arg.names) area <- arguments$area else area <- "no"  # tree and net.
+  # all:
+  if ("show.accu" %in% arg.names) show.accu <- arguments$show.accu else show.accu = TRUE
+  if ("area" %in% arg.names) area <- arguments$area else area <- "no"  # tree and net.
 
 
   ## Plotting functions:
   ## A. Frequency net (default):
-    if (plottype == "network") {
+  if (plottype == "network") {
 
-      if ("by" %in% arg.names) by <- arguments$by else by <- "cddc"
+    if ("by" %in% arg.names) by <- arguments$by else by <- "cddc"
 
-      plot_fnet(prev = obj$prev,
+    plot_fnet(prev = obj$prev,
+              sens = obj$sens, mirt = NA,
+              spec = obj$spec, fart = NA,
+              N = obj$N,
+              round = TRUE,
+              by = "cddc",
+              area = "sq",
+              p.lbl = "num",
+              show.accu = TRUE,
+              w.acc = 0.5,
+              title.lbl = obj$scen.lbl,
+              popu.lbl = obj$popu.lbl,
+              cond.true.lbl = obj$cond.true.lbl,
+              cond.false.lbl = obj$cond.false.lbl,
+              dec.pos.lbl = obj$dec.pos.lbl,
+              dec.neg.lbl = obj$dec.neg.lbl,
+              hi.lbl = obj$hi.lbl, mi.lbl = obj$mi.lbl, fa.lbl = obj$fa.lbl,
+              cr.lbl = obj$cr.lbl, col.txt = grey(0.01, alpha = 0.99), box.cex = 0.85,
+              col.boxes = pal, col.border = grey(0.33, alpha = 0.99), lwd = 1.5,
+              box.lwd = 1.5, col.shadow = col.sand.dark, cex.shadow = 0)
+
+  } # if (plottype == "network")...
+
+  ## B. Frequency tree:
+  if (plottype == "tree") {
+
+    if ("by" %in% arg.names) by <- arguments$by else by <- "cd"
+    if ("p.lbl" %in% arg.names) p.lbl <- arguments$p.lbl else p.lbl <- "mix"
+
+    plot_tree(prev = obj$prev,             # probabilities
+              sens = obj$sens, mirt = NA,
+              spec = obj$spec, fart = NA,  # was: num$fart,
+              N = obj$N,    # ONLY freq used (so far)
+              ## Options:
+              round = TRUE,  # Boolean: round freq (if computed), default: round = TRUE.
+              by = by,     # 4 perspectives: "cd" by condition, "dc" by decision.
+              area = area,   # 4 area types: "no" none (default), "sq" square, "hr" horizontal rectangles, "vr" vertical rectangles.
+              p.lbl = p.lbl, # 4 probability (edge) label types: "nam" names, "num" numeric, "mix" essential names + complement values (default), "min" minimal.
+              ## Compute and show accuracy info:
+              show.accu = TRUE,  # compute and show accuracy metrics
+              w.acc = .50,       # weight w for wacc (from 0 to 1)
+              ## Labels:
+              title.lbl = obj$scen.lbl,     # custom text labels
+              popu.lbl = obj$popu.lbl,
+              ## Condition labels:
+              cond.true.lbl = obj$cond.true.lbl,
+              cond.false.lbl = obj$cond.false.lbl,
+              ## Decision labels:
+              dec.pos.lbl = obj$dec.pos.lbl,
+              dec.neg.lbl = obj$dec.neg.lbl,
+              ## SDT combinations:
+              hi.lbl = obj$hi.lbl,
+              mi.lbl = obj$mi.lbl,
+              fa.lbl = obj$fa.lbl,
+              cr.lbl = obj$cr.lbl,
+              ## Box settings:
+              col.txt = grey(.01, alpha = .99),  # black
+              box.cex = .90,                     # relative text size
+              col.boxes = pal, # pal[c(1:9)],    # box colors (9 frequencies/boxes/colors)
+              col.border = grey(.33, alpha = .99),  # grey
+              ## Widths of arrows and box borders:
+              lwd = 1.6,      # width of arrows
+              box.lwd = 1.8,  # set to 0.001 to show boxes without borders (but =0 yields ERROR)
+              ## Shadows:
+              col.shadow = grey(.11, alpha = .99),  # dark grey
+              cex.shadow = 0  # [values > 0 show shadows]
+    )
+
+  } #  if (plottype == "tree")...
+
+
+  ## C. Mosaicplot:
+  if (plottype == "mosaicplot") {
+    plot_mosaic(prev = obj$prev,
                 sens = obj$sens, mirt = NA,
                 spec = obj$spec, fart = NA,
                 N = obj$N,
-                round = TRUE,
-                by = "cddc",
-                area = "sq",
-                p.lbl = "num",
-                show.accu = TRUE,
-                w.acc = 0.5,
+                vsplit = TRUE,
+                show.accu = TRUE, w.acc = 0.5,
                 title.lbl = obj$scen.lbl,
-                popu.lbl = obj$popu.lbl,
-                cond.true.lbl = obj$cond.true.lbl,
-                cond.false.lbl = obj$cond.false.lbl,
-                dec.pos.lbl = obj$dec.pos.lbl,
-                dec.neg.lbl = obj$dec.neg.lbl,
-                hi.lbl = obj$hi.lbl, mi.lbl = obj$mi.lbl, fa.lbl = obj$fa.lbl,
-                cr.lbl = obj$cr.lbl, col.txt = grey(0.01, alpha = 0.99), box.cex = 0.85,
-                col.boxes = pal, col.border = grey(0.33, alpha = 0.99), lwd = 1.5,
-                box.lwd = 1.5, col.shadow = col.sand.dark, cex.shadow = 0)
-    }
+                col.sdt = c(pal["hi"], pal["mi"], pal["fa"], pal["cr"]))
 
-  ## B. Frequency tree:
-    if (plottype == "tree") {
+  } # if (plottype == "mosaicplot")...
 
-      if ("by" %in% arg.names) by <- arguments$by else by <- "cd"
-      if ("p.lbl" %in% arg.names) p.lbl <- arguments$p.lbl else p.lbl <- "mix"
-
-      plot_tree(prev = obj$prev,             # probabilities
-                sens = obj$sens, mirt = NA,
-                spec = obj$spec, fart = NA,  # was: num$fart,
-                N = obj$N,    # ONLY freq used (so far)
-                ## Options:
-                round = TRUE,  # Boolean: round freq (if computed), default: round = TRUE.
-                by = by,     # 4 perspectives: "cd" by condition, "dc" by decision.
-                area = area,   # 4 area types: "no" none (default), "sq" square, "hr" horizontal rectangles, "vr" vertical rectangles.
-                p.lbl = p.lbl, # 4 probability (edge) label types: "nam" names, "num" numeric, "mix" essential names + complement values (default), "min" minimal.
-                ## Compute and show accuracy info:
-                show.accu = TRUE,  # compute and show accuracy metrics
-                w.acc = .50,       # weight w for wacc (from 0 to 1)
-                ## Labels:
-                title.lbl = obj$scen.lbl,     # custom text labels
-                popu.lbl = obj$popu.lbl,
-                ## Condition labels:
-                cond.true.lbl = obj$cond.true.lbl,
-                cond.false.lbl = obj$cond.false.lbl,
-                ## Decision labels:
-                dec.pos.lbl = obj$dec.pos.lbl,
-                dec.neg.lbl = obj$dec.neg.lbl,
-                ## SDT combinations:
-                hi.lbl = obj$hi.lbl,
-                mi.lbl = obj$mi.lbl,
-                fa.lbl = obj$fa.lbl,
-                cr.lbl = obj$cr.lbl,
-                ## Box settings:
-                col.txt = grey(.01, alpha = .99),  # black
-                box.cex = .90,                     # relative text size
-                col.boxes = pal, # pal[c(1:9)],    # box colors (9 frequencies/boxes/colors)
-                col.border = grey(.33, alpha = .99),  # grey
-                ## Widths of arrows and box borders:
-                lwd = 1.6,      # width of arrows
-                box.lwd = 1.8,  # set to 0.001 to show boxes without borders (but =0 yields ERROR)
-                ## Shadows:
-                col.shadow = grey(.11, alpha = .99),  # dark grey
-                cex.shadow = 0  # [values > 0 show shadows]
-      )
-
-    }
-
-  ## C. Curve:
-    if (plottype == "curve") {
-
-      if ("what" %in% arg.names) what <- arguments$what else what <- c("prev", "PPV", "NPV")
-      if ("show.points" %in% arg.names) show.points <- arguments$show.points else show.points <- TRUE
-
-      plot_curve(prev = obj$prev,             # probabilities (3 essential, 2 optional)
-                  sens = obj$sens, mirt = NA,
-                  spec = obj$spec, fart = NA,
-                  ## DVs:
-                  what = what,  # what curves?  Options: "acc", "ppod"
-                  what.col = pal,                  # colors for what.
-                  ## Options:
-                  show.points = show.points,  # show points at current prev?
-                  log.scale = FALSE,   # x-axis on log scale?
-                  title.lbl = obj$scen.lbl
-      )
-    }
 
   ## D. Iconarrays
-    if (plottype == "iconarray") {
+  if (plottype == "iconarray") {
 
-      if ("type" %in% arg.names) type <- arguments$type else type <- "array"
+    if ("type" %in% arg.names) type <- arguments$type else type <- "array"
 
-      plot_icons(prev = obj$prev,             # probabilities
-                 sens = obj$sens, mirt = NA,
-                 spec = obj$spec, fart = NA,  # was: num$fart,
-                 N = obj$N,    # ONLY freq used (so far)
-                 ## Key options: ##
-                 type = type,  # needs to be given if random position but nonrandom ident.
-                 # Types include: array, shuffled array, mosaic, equal, fillleft, filltop, scatter.
-                 ident.order = c("hi", "mi", "fa", "cr"),
-                 icon.colors = pal[c("hi", "mi", "fa", "cr")],  # use one color for each usual type.
-                 icon.types = 22,  # plotting characters; default square with border
-                 icon.border.col = grey(.10, .50),  # border color of icons
-                 icon.border.lwd = 1.5, # line width of icons
-                 transparency = .50,
-                 icon.size = NULL,
-                 block.d = NULL,  # distance between blocks (where applicable).
-                 border.d = 0.1,  # distance of icons to border.
+    plot_icons(prev = obj$prev,             # probabilities
+               sens = obj$sens, mirt = NA,
+               spec = obj$spec, fart = NA,  # was: num$fart,
+               N = obj$N,    # ONLY freq used (so far)
+               ## Key options: ##
+               type = type,  # needs to be given if random position but nonrandom ident.
+               # Types include: array, shuffled array, mosaic, equal, fillleft, filltop, scatter.
+               ident.order = c("hi", "mi", "fa", "cr"),
+               icon.colors = pal[c("hi", "mi", "fa", "cr")],  # use one color for each usual type.
+               icon.types = 22,  # plotting characters; default square with border
+               icon.border.col = grey(.10, .50),  # border color of icons
+               icon.border.lwd = 1.5, # line width of icons
+               transparency = .50,
+               icon.size = NULL,
+               block.d = NULL,  # distance between blocks (where applicable).
+               border.d = 0.1,  # distance of icons to border.
 
-                 # for classic icon arrays only:
-                 block.size.row = 10,
-                 block.size.col = 10,
-                 nblocks.row = NULL,
-                 nblocks.col = NULL,
+               # for classic icon arrays only:
+               block.size.row = 10,
+               block.size.col = 10,
+               nblocks.row = NULL,
+               nblocks.col = NULL,
 
-                 fill.array = "left",
-                 fill.blocks = "rowwise",
+               fill.array = "left",
+               fill.blocks = "rowwise",
 
-                 show.accu = TRUE, # Option for showing current accuracy metrics.
-                 w.acc = 0.50,
+               show.accu = TRUE, # Option for showing current accuracy metrics.
+               w.acc = 0.50,
 
-                 # labelling:
-                 title.lbl = obj$scen.lbl,
-                 type.lbls = obj[c("hi.lbl", "mi.lbl", "fa.lbl", "cr.lbl")])
+               # labelling:
+               title.lbl = obj$scen.lbl,
+               type.lbls = obj[c("hi.lbl", "mi.lbl", "fa.lbl", "cr.lbl")])
 
-    }
+  } #  if (plottype == "iconarray")...
 
-  ## E. Mosaicplot:
-    if (plottype == "mosaicplot") {
-      plot_mosaic(prev = obj$prev,
-                  sens = obj$sens, mirt = NA,
-                  spec = obj$spec, fart = NA,
-                  N = obj$N,
-                  vsplit = TRUE,
-                  show.accu = TRUE, w.acc = 0.5,
-                  title.lbl = obj$scen.lbl,
-                  col.sdt = c(pal["hi"], pal["mi"], pal["fa"], pal["cr"]))
-    }
+
+  ## E. Curve:
+  if (plottype == "curve") {
+
+    if ("what" %in% arg.names) what <- arguments$what else what <- c("prev", "PPV", "NPV")
+    if ("show.points" %in% arg.names) show.points <- arguments$show.points else show.points <- TRUE
+
+    plot_curve(prev = obj$prev,             # probabilities (3 essential, 2 optional)
+               sens = obj$sens, mirt = NA,
+               spec = obj$spec, fart = NA,
+               ## DVs:
+               what = what,  # what curves?  Options: "acc", "ppod"
+               what.col = pal,                  # colors for what.
+               ## Options:
+               show.points = show.points,  # show points at current prev?
+               log.scale = FALSE,   # x-axis on log scale?
+               title.lbl = obj$scen.lbl
+    )
+  } # if (plottype == "curve")...
 
   ## F. Plane:
-    if (plottype == "plane") {
+  if (plottype == "plane") {
 
-      if ("what" %in% arg.names) what <- arguments$what else what <- "PPV"
+    if ("what" %in% arg.names) what <- arguments$what else what <- "PPV"
 
-      plot_plane(prev = obj$prev,             # probabilities (3 essential, 2 optional)
-                 sens = obj$sens, mirt = NA,
-                 spec = obj$spec, fart = NA,
-                 ## DVs:
-                 what = what, # what plane?  Options: "PPV", "NPV", "acc", "ppod".
-                 ## Options:
-                 what.col = pal,     # color for what.
-                 step.size = .05,    # resolution of matrix (sens.range and spec.range)
-                 show.point = TRUE,  # show point on plane?
-                 ## Main persp() options [adjustable]:
-                 theta = -45,
-                 phi = 0,
-                 ## Text:
-                 title.lbl = txt$scen.lbl, # plot title label
-                 cex.lbl = .85 # scale size of text labels (e.g., on axes, legend, margin text)
-                 )
-    }
+    plot_plane(prev = obj$prev,             # probabilities (3 essential, 2 optional)
+               sens = obj$sens, mirt = NA,
+               spec = obj$spec, fart = NA,
+               ## DVs:
+               what = what, # what plane?  Options: "PPV", "NPV", "acc", "ppod".
+               ## Options:
+               what.col = pal,     # color for what.
+               step.size = .05,    # resolution of matrix (sens.range and spec.range)
+               show.point = TRUE,  # show point on plane?
+               ## Main persp() options [adjustable]:
+               theta = -45,
+               phi = 0,
+               ## Text:
+               title.lbl = txt$scen.lbl, # plot title label
+               cex.lbl = .85 # scale size of text labels (e.g., on axes, legend, margin text)
+    )
+  } # if (plottype == "plane")...
 
   # Include other plottypes here.
 
@@ -231,10 +236,10 @@ plot.riskyr <- function(obj,
 # plot(scenarios.lst$scen25, plottype = "wetwork")
 
 
-## -----------------------------------------------
-## (2) summary.riskyr function:
+## (2) summary.riskyr function: ------------------
 
-## First, create a summary object: ---------------
+## (A) Create a summary object: ------------------
+
 summary.riskyr <- function(obj, summarize = "all", ...) {
 
   obj.sum <- list()
@@ -301,7 +306,8 @@ summary.riskyr <- function(obj, summarize = "all", ...) {
 }
 
 
-# Then, create a corresponding print function: --------------------
+## (B) Create corresponding print function: ----------
+
 print.summary.riskyr <- function(obj) {
 
   ## This is always printed:
@@ -352,7 +358,10 @@ print.summary.riskyr <- function(obj) {
 # summary(scenario3)
 # summary(scenario2, summarize = "probs")
 
-# Function to create riskyr scenarios: --------------------------------------------
+
+## -----------------------------------------------
+## (3) Function to create riskyr scenarios: ------
+
 riskyr <- function (scen.lbl = txt$scen.lbl, scen.lng = txt$scen.lng,
                     scen.txt = txt$scen.txt, popu.lbl = txt$popu.lbl,
                     cond.lbl = txt$cond.lbl,
@@ -406,6 +415,7 @@ riskyr <- function (scen.lbl = txt$scen.lbl, scen.lng = txt$scen.lng,
 # #   paste0(
 # #     paste0(names(scenarios.lst$scen1), " = ", names(scenarios.lst$scen1)),
 # #     collapse = ", "))
+
 
 ## -----------------------------------------------
 ## (3) Define an object with a list of riskyr objects:
