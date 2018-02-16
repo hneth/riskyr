34a0db7713d07827703efcf1bb1cfc53bd2d8b7b
hneth <hansneth@gmail.com>
2018-02-16 11:10:03 +0100

    add vignette information

--- vignettes/E_riskyr_primer.R
+++ vignettes/E_riskyr_primer.R
@@ -3,7 +3,9 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
+# initialize some stuff:
 library("riskyr")  # loads the package
+op <- par(no.readonly = TRUE)   
 
 ## ----load_riskyr---------------------------------------------------------
 library("riskyr")  # loads the package
@@ -17,11 +19,11 @@ my.scenario <- riskyr(prev = .45,
 ## ----create_scenario_custom----------------------------------------------
 # Create a customized scenario: 
 my.scenario <- riskyr(scen.lbl = "Identifying reoffenders", 
-                      popu.lbl = "inmates", 
+                      popu.lbl = "prison inmates", 
                       cond.lbl = "reoffending",
                       cond.true.lbl = "offends again", cond.false.lbl = "does not offend again",
                       dec.lbl = "test result",
-                      dec.pos.lbl = "predict to reoffend", dec.neg.lbl = "predict to not reoffend",
+                      dec.pos.lbl = "predict to\nreoffend", dec.neg.lbl = "predict to\nnot reoffend",
                       hi.lbl = "reoffender found", mi.lbl = "reoffender missed",
                       fa.lbl = "false accusation", cr.lbl = "correct release",
                       prev = .45,  # prevalence of being a reoffender. 
@@ -32,7 +34,7 @@ my.scenario <- riskyr(scen.lbl = "Identifying reoffenders",
                       scen.src = "(a ficticious example)")
 
 ## ----fnet, include = FALSE, fig.width = 7.2, fig.height = 6--------------
-plot(my.scenario, plot.type = "fnet")
+plot(my.scenario, plot.type = "fnet", round = FALSE)
 
 ## ----icons, fig.width = 7.2, fig.height = 4.8----------------------------
 plot(my.scenario, plot.type = "icons")
@@ -40,67 +42,60 @@ plot(my.scenario, plot.type = "icons")
 ## ----full_summary--------------------------------------------------------
 summary(my.scenario)
 
-## ----summary_prob--------------------------------------------------------
+## ----summary_prob, include = FALSE---------------------------------------
 summary(my.scenario, summarize = "prob")
 
-## ----tree, fig.width=7, fig.height=6-------------------------------------
-plot(my.scenario, plot.type = "tree", by = "dc")
+## ----tree, fig.width = 7.2, fig.height = 5.5-----------------------------
+plot(my.scenario, plot.type = "tree", by = "dc")  # plot tree diagram (splitting N by decision)
 
-## ----plotting_curve, fig.width=7, fig.height=5.5-------------------------
+## ----plotting_curve, fig.width = 7, fig.height = 5.5---------------------
 plot(my.scenario, plot.type = "curve")
 
-## ----scenario_table, echo = FALSE, results = 'asis'----------------------
+## ----scenario_table, echo = FALSE, results = "asis"----------------------
 library(knitr)
 scen.table <- df.scenarios[-1,
-                           c("scen.lbl", "cond.lbl", "dec.lbl", "N", "prev",
-                             "sens", "spec", "fart", "scen.src")]
-scen.table[, -c(1:3, 9)] <- round(scen.table[, -c(1:3, 9)], 3)
-names(scen.table) <- c("Scenario", "Condition", "Decision",
-                       "N", "Prevalence", "Sensitivity", "Specificity", "False alarm rate",
-                       "Source")
+                           c("scen.lbl", "cond.lbl", "N", "prev",
+                             "sens", "spec", "fart")]
+scen.table[, -c(1:2)] <- round(scen.table[, -c(1:2)], 3)
+names(scen.table) <- c("Scenario", "Condition", "N", "prev", "sens", "spec", "fart")
 kable(scen.table)
 
-## ----example_use_a, fig.width=7, fig.height=5.5--------------------------
-## (a) PSA screening with high prevalence: ----------
-s21 <- scenarios$n21  # assign pre-defined Scenario_21 to s
+## ----s21_select----------------------------------------------------------
+s21 <- scenarios$n21  # assign pre-defined Scenario 21 to s21.
 
-## ----summary_s21---------------------------------------------------------
-summary(s21) # shows all scenario information.
+## ----s21_info------------------------------------------------------------
+# Show basic scenario information: 
+s21$scen.lbl  # shows descriptive label:
+s21$cond.lbl  # shows current condition:
+s21$dec.lbl   # shows current decision:
+s21$popu.lbl  # shows current population:
+s21$scen.apa  # shows current source: 
 
-## ----summary_s21_freq----------------------------------------------------
-summary(s21, summarize = "freq") # shows frequencies. 
+## ----s21_summary---------------------------------------------------------
+summary(s21) # summarizes key scenario information:
 
-## ----s21_net, fig.width=7, fig.height=6----------------------------------
-plot(s21) # plots a network diagram (by default)
-
-## ----s21_icons, fig.width = 7, fig.height = 5----------------------------
+## ----s21_icons, fig.width = 7.2, fig.height = 4.5------------------------
 plot(s21, plot.type = "icons", cex.lbl = 0.75)
 
-## ----s21_curve, fig.width = 7, fig.height = 5----------------------------
+## ----s21_fnet, fig.width = 7.2, fig.height = 6.5-------------------------
+plot(s21, plot.type = "fnet", area = "sq")
+
+## ----s21_curve, fig.width = 7.2, fig.height = 5--------------------------
 plot(s21, plot.type = "curve", what = "all")
 
-## ----s22_summary---------------------------------------------------------
-s22 <- scenarios$n22  # assign pre-defined Scenario_22 to s22. 
+## ----s21_planes, results = "hold", fig.width = 7.8, fig.height = 4.2-----
+op <- par(no.readonly = TRUE)  # save plot settings.
+par(mfrow = c(1, 2))           # 1 row with 2 plots:
 
-summary(s22)
-
-## ----example_use_b, fig.width=7.5, fig.height=5.5------------------------
-op <- par(no.readonly = TRUE)
-par(mfrow = c(1,2))  # set plotting space for direct comparison. 
-
-## Contrast two versions: 
+## Plot plane of PPV and NPV as functions of sens and spec (for given prev): 
 plot(s21, plot.type = "plane", what = "PPV", cex.lbl = 0.75)
-plot(s22, plot.type = "plane", what = "PPV", cex.lbl = 0.75)
-
-
-## ----examples_NPV, fig.width=7.5, fig.height=5.5-------------------------
-op <- par(no.readonly = TRUE)
-par(mfrow = c(1,2))  # set plotting space for direct comparison. 
-
-## Contrast two versions: 
 plot(s21, plot.type = "plane", what = "NPV", cex.lbl = 0.75)
-plot(s22, plot.type = "plane", what = "NPV", cex.lbl = 0.75)
+par(op)  # reset plot settings.
 
-## ----reset_par, echo = FALSE---------------------------------------------
-par(op)
+## ----s22_summary---------------------------------------------------------
+s22 <- scenarios$n22  # assign pre-defined Scenario 22 to s22. 
+
+# Show basic scenario information: 
+s22$scen.lbl  # shows descriptive label:
+s22$popu.lbl  # shows current population:
 
