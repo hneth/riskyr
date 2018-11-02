15529abc131796083f94b7ebf59699d606674be1
hneth <hansneth@gmail.com>
2018-11-02 18:10:44 +0100

    increment version

--- R/start_riskyr.R
+++ R/start_riskyr.R
@@ -87,7 +87,7 @@ start_riskyr <- function(...) {
 
   if (dice == 1) {
     packageStartupMessage(" ")
-    packageStartupMessage("Running riskyr (v0.1.920)...")
+    packageStartupMessage("Running riskyr (v0.1.921)...")
     packageStartupMessage(" ")
   }
 
