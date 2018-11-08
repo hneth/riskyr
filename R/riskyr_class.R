a399e9da19b943b6fb41db09cdd0fc852c0b7175
nigradwohl <nico.gradwohl@uni.kn>
2018-11-08 18:27:30 +0100

    table method prototype

--- R/riskyr_class.R
+++ R/riskyr_class.R
@@ -898,14 +898,14 @@ riskyr_table <- function(object) {
 
   ## for user-defined display an additional flexible matrix may be used:
   cnd_dec_lab <- with(object,
-                      matrix(c(hi.lbl, mi.lbl, cond.true.lbl, "Sensitivity", "False positive rate",
-                               fa.lbl, cr.lbl, cond.false.lbl, "False negative rate", "Specificity",
-                               dec.pos.lbl, dec.neg.lbl, "N", "Proportion of positive decisions",
-                               "Proportion of negative decisions",
-                               "PPV", "FOR", "Prevalence", "Accuracy", "",
+                      matrix(c("hi", "mi", "cond.true", "Sens", "FPR",
+                               "fa", "cr", "cond.false", "FNR", "Spec",
+                               "dec.pos", "dec.neg", "N", "ppod", "ppnd",
+                               "PPV", "FOR", "Prev", "Acc", "",
                                "FDR", "NPV", "1-prev", "", ""),
                              nrow = 5, ncol = 5)
   )
+
 
   ## Basic: dim 1 (cnd) and dim 2 (dec)
   probs <- comp_prob_prob(prev = object$prev, sens = object$sens, spec = object$spec)
@@ -937,8 +937,6 @@ riskyr_table <- function(object) {
 
 ## 3b. nice_table function: --------------------
 
-smr <- riskyr_table(tst)
-
 nice_tab <- function(smr, space = 2) {  # TODO: Give object class summary.
 
   max_nchar <- max(nchar(smr))
@@ -950,7 +948,7 @@ nice_tab <- function(smr, space = 2) {  # TODO: Give object class summary.
   addspace <- sapply(naddspace, function(x) {paste0(rep(" ", times = x), collapse = "")})
 
   pad_vec <- rep(padding, length(smr))
-  pad_vec[1] <- paste0(padding, " ", collapse = "")
+  #pad_vec[1] <- paste0(padding, " ", collapse = "")
 
   cells <- paste0(pad_vec, smr, addspace, padding)
   mcells <- matrix(cells, nrow = 5, ncol = 5)  # bring back to form.
@@ -960,7 +958,7 @@ nice_tab <- function(smr, space = 2) {  # TODO: Give object class summary.
   csep <- paste0(rep("-", cellwd), collapse  = "")  # horizontal separator for each cell.
   hsep <- paste0(paste0(rep(csep, 5), collapse = "|"), "\n")  # horizontal separator.
 
-  tab <- rbind(ctab, rep(hsep, length(ctab)))
+  tab <- c("\n", hsep, rbind(ctab, rep(hsep, length(ctab))))
 
   cat(tab)
 
@@ -973,11 +971,28 @@ nice_tab <- function(smr, space = 2) {  # TODO: Give object class summary.
 
 ## 3c. table.riskyr method: --------------------
 ## Takes a riskyr object and prints out a nice table:
-table.riskyr <- function(x, sep = " = ") {
+table.riskyr <- function(x, sep = " = ", space = 2) {
 
   tab <- riskyr_table(x)  # convert riskyr to table.
-  tab$num
-  tabp <- matrix(paste(tab$lbl, tab$num, sep = sep), nrow = 5, ncol = 5)
+
+  nums <- round(tab$num, 2)
+  lbl <- tab$lbl
+  nums[is.na(nums)] <- ""
+  out <- matrix(paste0(lbl, sep, nums), nrow = 5, ncol = 5)
+  out[out == sep] <- ""  # replace empty cells.
+
+  class(out) <- "table.riskyr"
+
+  return(out)
+
+
+  #
+  #             ## TODO: Allow the transposed forms as well.
+  #
+  #             ix <- ifelse(relf, 2, 1)  # decide whether to report relative frequencies.
+  #             head <- ifelse(relf, "relf", "freq")
+  #             cat(head, "\n")
+  #             return(out)
 
 }
 
@@ -985,7 +1000,10 @@ table.riskyr <- function(x, sep = " = ") {
 # tst <- riskyr(hi = 1, mi = 3, fa = 4, cr = 5)
 #
 
-
+## 3d. print.table.riskyr method: ----------------
+print.table.riskyr <- function(x, space = 2) {
+  nice_tab(x, space = space)
+}
 
 ## 4. print.summary.riskyr function: ------------------
 
