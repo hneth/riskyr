45ad5baa513809a0a2fb343e445074cb31d7b99b
hneth <hansneth@gmail.com>
2018-11-23 09:25:30 +0100

    update vignette (from 11 to 13 probabilities) and new plot_prism function

--- vignettes/B_data_formats.R
+++ vignettes/B_data_formats.R
@@ -4,7 +4,7 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----prism_plot_1, message = FALSE, fig.width = 7.2, fig.height = 6, fig.show = 'asis', fig.cap = "A network diagram showing frequencies as nodes and probabilities as edges linking nodes."----
+## ----prism_plot_1, message = FALSE, fig.width = 7, fig.height = 5.5, fig.show = 'asis', fig.cap = "A prism plot showing frequencies as nodes and probabilities as edges linking nodes."----
 library("riskyr") # load the "riskyr" package
 
 plot_prism(prev = .01, sens = .80, spec = NA, fart = .096,  # 3 essential probabilities
@@ -13,10 +13,11 @@ plot_prism(prev = .01, sens = .80, spec = NA, fart = .096,  # 3 essential probab
            p_lbl = "abb",  # show abbreviated names of probabilities on edges
            title_lbl = "Example")
 
-## ----prism_plot_2, fig.width = 7.2, fig.height = 6, fig.show = 'asis', fig.cap = "A simple network diagram that shows how probabilities can be computed as ratios of frequencies."----
+## ----prism_plot_2, fig.width = 7, fig.height = 5, fig.show = 'asis', fig.cap = "A prism plot showing how probabilities can be computed as ratios of frequencies."----
 plot_prism(prev = .50, sens = .80, spec = .60,  # 3 essential probabilities
-           N = 10,         # 1 frequency
-           area = "no",    # all boxes have the same size
+           N = 10,         # population frequency
+           scale = "f",    # scale by frequency, rather than probability ("p") 
+           area = "sq",    # boxes as squares, with sizes scaled by current scale  
            p_lbl = "num",  # show numeric probability values on edges
            title_lbl = "Probabilities as ratios of frequencies")
 
