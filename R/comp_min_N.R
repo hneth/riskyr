2ece243683fe16282dc03c1eb4cbcee62aa4c683
hneth <hansneth@gmail.com>
2018-12-06 20:24:45 +0100

    retired comp_freq_prob.R as it only contained 1 function comp_min_N.R (which is now saved under its own name)

--- R/comp_min_N.R
+++ R/comp_min_N.R
@@ -1,77 +1,13 @@
-## comp_freq_prob.R | riskyr
-## 2018 10 26
-## Compute frequencies from probabilities:
+## comp_min_N.R | riskyr
+## 2018 12 06
+## Compute minimum population size N (given prob)
 ## -----------------------------------------------
 
-## Table of current terminology: -----------------
-
-# Probabilities (13+):              Frequencies (11):
-# -------------------               ------------------
-# (A) by condition:
-
-# non-conditional:                          N
-# prev*                           cond.true | cond.false (columns)
-
-# conditional:
-# sens* = hit rate = TPR                hi* = TP
-# mirt  = miss rate = FNR               mi* = FN
-# fart  = false alarm rate = FPR        fa* = FP
-# spec* = true negative rate = TNR      cr* = TN
-
-# [Note: *...is essential]
-
-
-# (B) by decision:                 Combined frequencies:
-
-# non-conditional:
-# ppod = proportion of dec.pos     dec.pos | dec.neg (rows)
-#                                  dec.cor | dec.err (diagonal)
-
-# conditional:
-# PPV = precision
-# FDR = false detection rate
-# FOR = false omission rate
-# NPV = neg. pred. value
-
-# (C) by accuracy/correspondence of decision to condition (see accu):
-
-# acc  = overall accuracy (probability/proportion correct decision)
-# p_acc_hi = p(hi|acc)  # aka. acc-hi  "p(hi | dec.cor)"
-# p_err_fa = p(fa|err)  # aka. err-fa  "p(fa | dec.err)"
-
-# Other measures of accuracy (in accu):
-# wacc = weighted accuracy
-# mcc  = Matthews correlation coefficient
-# f1s  = harmonic mean of PPV and sens
-
-# err = error rate = (1 - acc)
-
-
-## Data flow: Two basic directions: --------------
-
-## (1) Probabilities ==> frequencies:
-##     Bayesian: based on 3 essential probabilities:
-##   - given:   prev;  sens, spec
-##   - derived: all other values
-
-## (2) Frequencies ==> probabilities:
-##     Frequentist: based on 4 essential natural frequencies:
-##   - given:   N = hi, mi, fa, cr
-##   - derived: all other values
-
-
-## 2 main functions convert between formats: ----------
-
-## a. comp_freq_prob: Computes freq from prob (in comp_xxxx_prob.R)
-## b. comp_prob_freq: Computes prob from freq (in comp_prob_freq.R)
-
-## ad (1) from prob ==> to freq: ----------
-
-## (1) Determine a suitable population size N: --------
+## Compute suitable minimum population size value N: --------
 
 ## Criterion: All 4 SDT cells should have a minimal frequency of min.freq:
 
-## comp_min_N: Compute suitable minimum population size value N ------
+## comp_min_N: Documentation ------
 
 #' Compute a suitable minimum population size value N.
 #'
@@ -82,13 +18,13 @@
 #' \code{\link{prev}}, \code{\link{sens}}, and \code{\link{spec}} (\code{spec = 1 - \link{fart}}).
 #'
 #' Using this function helps avoiding excessively small decimal values in categories
-#' (especially \code{\link{hi}}, \code{\link{mi}}, \code{\link{fa}}, \code{\link{cr}})
+#' -- especially \code{\link{hi}}, \code{\link{mi}}, \code{\link{fa}}, \code{\link{cr}} --
 #' when expressing combinations of conditions and decisions as natural frequencies.
 #' As values of zero (0) are tolerable, the function only increases \code{\link{N}}
 #' (in powers of 10) while the current value of any frequency (cell in confusion table or
 #' leaf of a frequency tree) is positive but below \code{min.freq}.
 #'
-#' By default, \code{\link{{comp_freq_prob}} and \code{\link{comp_freq}}
+#' By default, \code{\link{comp_freq_prob}} and \code{\link{comp_freq}}
 #' round frequencies to nearest integers to avoid decimal values in
 #' \code{\link{freq}} (i.e., \code{round = TRUE} by default).
 #' Using the option \code{round = FALSE} turns off rounding.
@@ -137,6 +73,8 @@
 #' \code{\link{comp_prob_prob}} computes current probability information from (3 essential) probabilities.
 #'
 #' @export
+
+## comp_min_N: Definition ------
 
 comp_min_N <- function(prev, sens, spec,  # 3 essential probabilities
                        min.freq = 1) {
@@ -192,7 +130,8 @@ comp_min_N <- function(prev, sens, spec,  # 3 essential probabilities
 
 ## (*) Done: ----------
 
-## - Clean up code.  [2018 08 30]
+## - Clean up code.                                         [2018 08 30]
+## - Rename file from "comp_freq_prob.R" to "comp_min_N.R". [2018 12 06]
 
 ## (+) ToDo: ----------
 
