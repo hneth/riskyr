a0bc64974337a9a485ed64e3b49b07bf8649c389
hneth <hansneth@gmail.com>
2018-12-21 16:15:22 +0100

    update and simplify vignettes

--- vignettes/C_confusion_matrix.R
+++ vignettes/C_confusion_matrix.R
@@ -12,7 +12,7 @@ plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
          p_lbl = "def", title_lbl = "Scenario 1")
 
 ## Plot an existing riskyr scenario: ----- 
-# s <- scenarios$n21
+# s <- scenarios$n1
 # plot(s, type = "tab", p_lbl = "def")
 
 ## ----plot_tab_cdac, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Arranging a 2x2 confusion table by condition and by accuracy."----
--- vignettes/D_functional_perspectives.R
+++ vignettes/D_functional_perspectives.R
@@ -6,28 +6,34 @@ knitr::opts_chunk$set(
 
 library("riskyr")  # load the "riskyr" package
 
-## ----plot_curve_1, fig.width = 7.2, fig.height = 5, fig.show = 'asis', fig.cap = "Curves that show PPV and NPV as a function of an prevalence (for given values of sensitivity and specificity) in the original mammography screening scenario."----
+## ----plot_curve_1, fig.width = 7.0, fig.height = 5, fig.show = 'asis', fig.cap = "Showing PPV and NPV as a function of prevalence (for a prevalance of 1% and given values of sensitivity and specificity) in the original mammography screening scenario."----
 plot_curve(prev = .01, sens = .80, spec = (1 - .096), 
            what = c("prev", "PPV", "NPV"), 
-           title.lbl = "Mammography screening", cex.lbl = .8)
+           title_lbl = "Mammography screening", cex.lbl = .8)
 
-## ----plot_curve_2, fig.width = 7.2, fig.height = 5, fig.show = 'asis', fig.cap = "Curves that show PPV and NPV as a function of an prevalence (for given values of sensitivity and specificity) when assuming an increased prevalence of 60%."----
-high.prev <- .60   # assume increased prevalence due to BRCA1 mutation
+## ----plot_curve_2, eval = FALSE, fig.width = 7.0, fig.height = 5, fig.show = 'asis', fig.cap = "Showing PPV and NPV as a function of prevalence (for an increased prevalence of 60% and given values of sensitivity and specificity)."----
+#  high.prev <- .60   # assume increased prevalence due to BRCA1 mutation
+#  
+#  plot_curve(prev = high.prev, sens = .80, spec = (1 - .096),
+#             what = c("prev", "PPV", "NPV"),
+#             title_lbl = "Mammography screening (BRCA1 mutation)", cex.lbl = .80)
 
-plot_curve(prev = high.prev, sens = .80, spec = (1 - .096), 
-           what = c("prev", "PPV", "NPV"), 
-           title.lbl = "Mammography screening (BRCA1 mutation)", cex.lbl = .8)
-
-## ----plot_curve_3, fig.width = 7.2, fig.height = 5, fig.show = 'asis', fig.cap = "Curves that show PPV and NPV as a function of an prevalence (for given values of sensitivity and specificity) when assuming an increased prevalence of 60%."----
+## ----plot_curve_3, fig.width = 7.0, fig.height = 5, fig.show = 'asis', fig.cap = "Curves that show PPV/NPV, ppod, and acc as a function of an prevalence (for given values of sensitivity and specificity) when assuming an increased prevalence of 60% and an uncertainty range of 5%."----
 high.prev <- .60   # assume increased prevalence due to BRCA1 mutation
 
 plot_curve(prev = high.prev, sens = .80, spec = (1 - .096), 
            what = c("prev", "PPV", "NPV", "ppod", "acc"), 
-           title.lbl = "Mammography screening (BRCA1 mutation)", cex.lbl = .8)
+           title_lbl = "Mammography screening (BRCA1 mutation)", uc = .05, cex.lbl = .80)
 
-## ----plot_plane, fig.width = 6, fig.height = 4.5, fig.show = 'asis', fig.cap = "Planes that show PPV, NPV, the proportion of positive predictions (ppod) and overall accuracy (acc) as a function of sensitivity and specificity (for given prevalence)."----
-plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "PPV",  title.lbl = "A. Mammography (BRCA1)", cex.lbl = .8)
-plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "NPV",  title.lbl = "B. Mammography (BRCA1)", cex.lbl = .8)
-plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "ppod", title.lbl = "C. Mammography (BRCA1)", phi = 45, cex.lbl = .8)
-plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "acc",  title.lbl = "D. Mammography (BRCA1)", cex.lbl = .8)
+## ----plot_plane_PPV, eval = TRUE, fig.width = 5, fig.height = 4, fig.show = 'asis', fig.cap = "Plane showing the positive predictive value (PPV) as a function of sensitivity and specificity for a given prevalence."----
+plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "PPV",  
+           title_lbl = "A. Mammography (BRCA1)", cex.lbl = .8)
+
+## ----plot_planes_else, eval = FALSE, fig.width = 6, fig.height = 4.5, fig.show = 'asis', fig.cap = "Planes showing NPV, the proportion of positive predictions (ppod), and overall accuracy (acc), as a function of sensitivity and specificity for a given prevalence."----
+#  plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "NPV",
+#             title_lbl = "B. Mammography (BRCA1)", cex.lbl = .8)
+#  plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "ppod", what_col = "firebrick",
+#             title_lbl = "C. Mammography (BRCA1)", phi = 45, cex.lbl = .8)
+#  plot_plane(prev = high.prev, sens = .80, spec = (1 - .096), what = "acc",  what_col = "forestgreen",
+#             title_lbl = "D. Mammography (BRCA1)", cex.lbl = .8)
 
