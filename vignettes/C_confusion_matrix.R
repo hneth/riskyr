79a74ffaf36d7ce648dcc562b6bbb7fb528cf81b
hneth <hansneth@gmail.com>
2019-01-02 12:19:39 +0100

    update vignettes (to pass CRAN checks)

--- vignettes/C_confusion_matrix.R
+++ vignettes/C_confusion_matrix.R
@@ -6,17 +6,17 @@ knitr::opts_chunk$set(
 
 library("riskyr")  # load the "riskyr" package
 
-## ----plot_tab_demo, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Example of a 2x2 confusion table in `riskyr`."----
-## Plot table from basic input parameters: ----- 
-plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
-         p_lbl = "def", title_lbl = "Scenario 1")
+## ----plot_tab_demo, eval = FALSE, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Example of a 2x2 confusion table in `riskyr`."----
+#  # Plot table from basic input parameters: -----
+#  plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
+#           p_lbl = "def", title_lbl = "Scenario 1")
+#  
+#  # Plot an existing riskyr scenario: -----
+#  s <- scenarios$n1
+#  plot(s, type = "tab", p_lbl = "def")
 
-## Plot an existing riskyr scenario: ----- 
-# s <- scenarios$n1
-# plot(s, type = "tab", p_lbl = "def")
-
-## ----plot_tab_cdac, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Arranging a 2x2 confusion table by condition and by accuracy."----
-plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
-         by = "cdac", p_split = "h", 
-         p_lbl = "def", title_lbl = "Scenario 2")
+## ----plot_tab_cdac, eval = FALSE, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Arranging a 2x2 confusion table by condition and by accuracy."----
+#  plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
+#           by = "cdac", p_split = "h",
+#           p_lbl = "def", title_lbl = "Scenario 2")
 
