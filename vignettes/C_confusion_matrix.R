1d5e37af8fce6206757aa083d8c5fab773a88187
hneth <hansneth@gmail.com>
2019-01-23 10:25:00 +0100

    bug fix

--- vignettes/C_confusion_matrix.R
+++ vignettes/C_confusion_matrix.R
@@ -7,13 +7,17 @@ knitr::opts_chunk$set(
 library("riskyr")  # load the "riskyr" package
 
 ## ----plot_tab_demo, eval = TRUE, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Example of a 2x2 confusion table in `riskyr`."----
-# Plot table from basic input parameters: ----- 
+## (1) Plot table from basic input parameters: ----- 
 plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
-         p_lbl = "def", title_lbl = "Scenario 1")
+         p_lbl = "def") # show condition probabilies (by default)
 
-## Plot an existing riskyr scenario: ----- 
-# s <- scenarios$n1
-# plot(s, type = "tab", p_lbl = "def")
+## Alternative version (with horizontal p):
+# plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
+#          p_lbl = "def", p_split = "h") # show decision probabilities
+
+## (2) Plot an existing riskyr scenario: ----- 
+# s1 <- scenarios$n1  # identify s1 from scenarios
+# plot(s1, type = "tab", p_lbl = "def")
 
 ## ----plot_tab_cdac, eval = TRUE, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Arranging a 2x2 confusion table by condition and by accuracy."----
 plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
