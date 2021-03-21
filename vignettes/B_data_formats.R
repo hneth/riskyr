4d7613c12d4b0e1bcf0ab67eb81375d95537861b
hneth <hansneth@gmail.com>
2021-03-21 11:57:48 +0100

    update vignettes

--- vignettes/B_data_formats.R
+++ vignettes/B_data_formats.R
@@ -4,7 +4,7 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----prism_plot_1, message = FALSE, fig.width = 7, fig.height = 5.5, fig.show = 'asis', fig.cap = "A prism plot showing frequencies as nodes and probabilities as edges linking nodes."----
+## ----prism-plot-1, message = FALSE, fig.width = 6, fig.height = 5, fig.show = 'asis', fig.cap = "A prism plot showing frequencies as nodes and probabilities as edges linking nodes."----
 library("riskyr") # load the "riskyr" package
 
 plot_prism(prev = .01, sens = .80, spec = NA, fart = .096,  # 3 essential probabilities
@@ -13,11 +13,11 @@ plot_prism(prev = .01, sens = .80, spec = NA, fart = .096,  # 3 essential probab
            p_lbl = "abb",  # show abbreviated names of probabilities on edges
            title_lbl = "Example")
 
-## ----prism_plot_2, fig.width = 7, fig.height = 5, fig.show = 'asis', fig.cap = "A prism plot showing how probabilities can be computed as ratios of frequencies."----
+## ----prism-plot-2, fig.width = 6, fig.height = 5, fig.show = 'asis', fig.cap = "A prism plot showing how probabilities can be computed as ratios between frequencies."----
 plot_prism(prev = .50, sens = .80, spec = .60,  # 3 essential probabilities
            N = 10,         # population frequency
            scale = "f",    # scale by frequency, rather than probability ("p") 
            area = "sq",    # boxes as squares, with sizes scaled by current scale  
            p_lbl = "num",  # show numeric probability values on edges
-           title_lbl = "Probabilities as ratios of frequencies")
+           title_lbl = "Probabilities as ratios between frequencies")
 
--- vignettes/C_confusion_matrix.R
+++ vignettes/C_confusion_matrix.R
@@ -6,7 +6,7 @@ knitr::opts_chunk$set(
 
 library("riskyr")  # load the "riskyr" package
 
-## ----plot_tab_demo, eval = TRUE, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Example of a 2x2 confusion table in **riskyr**."----
+## ----plot-tab-demo, eval = TRUE, fig.width = 6, fig.height = 4.5, fig.show = 'hold', fig.cap = "Example of a 2x2 confusion table in **riskyr**."----
 ## (1) Plot table from basic input parameters: ----- 
 plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
          p_lbl = "def") # show condition probabilies (by default)
@@ -19,7 +19,7 @@ plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
 # s1 <- scenarios$n1  # identify s1 from scenarios
 # plot(s1, type = "tab", p_lbl = "def")
 
-## ----plot_tab_cdac, eval = TRUE, fig.width = 7, fig.height = 5, fig.show = 'hold', fig.cap = "Arranging a 2x2 confusion table by condition and by accuracy."----
+## ----plot-tab-cdac, eval = TRUE, fig.width = 6, fig.height = 4.5, fig.show = 'hold', fig.cap = "Arranging a 2x2 confusion table by condition and by accuracy."----
 plot_tab(prev = .05, sens = .75, spec = .66, N = 1000,
          by = "cdac", p_split = "h", 
          p_lbl = "def", title_lbl = "Scenario 2")
