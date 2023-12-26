0418fa1447f75416a2d9ece19655f8fd66da6b4e
hneth <hansneth@gmail.com>
2023-12-26 10:31:45 +0100

    add parameters and (recursive) functions

--- R/cum_risk.R
+++ R/cum_risk.R
@@ -2,21 +2,34 @@
 ## 2023 12 25
 ## Compute cumulative risks
 
-# Recursive application of repeated risk r:
+
+# Parameters: ----
+
+# r ... risk
+# i ... periods/rounds/times
+#
+# ev ... events
+# pc ... percent/population
+
+
+# Recursive application of repeated risk r: ------
+
+# apply_risk() as recursive partition of population pc: ----
 
 apply_risk <- function(pc = 100, ev = 0, r, i){
 
   # cat(i, ": ", pc, "\n")
 
 
-  if (i == 0){
+  if (i == 0){ # stop:
 
-    print(paste0(ev, ": ", pc))
+    # print(paste0(ev, ": ", pc))
 
-  } else {
+  } else { # recurse:
 
-      j <- i - 1
-      pc <- c(apply_risk(pc * r, ev + 1, r, j), apply_risk(pc * (1 - r), ev, r, j))
+    j <- (i - 1)  # simplify
+
+    pc <- c(apply_risk(pc * r, ev + 1, r, j), apply_risk(pc * (1 - r), ev, r, j))
 
   }
 
@@ -28,6 +41,75 @@ apply_risk <- function(pc = 100, ev = 0, r, i){
 # # Check:
 # apply_risk(pc = 100, ev = 0, r = .25, i = 4)
 
+
+# Separately for p and ev: ------
+
+# A: comp_p: Probabilities ----
+
+comp_p <- function(p = 100, r, i){
+
+  if (i == 0){ # stop:
+
+    # print(paste0(p))
+
+  } else { # recurse:
+
+    p <- c(comp_p(p * r, r, (i - 1)), comp_p(p * (1 - r), r, (i - 1)))
+
+  }
+
+  return(p)
+
+}
+
+# # Check:
+# comp_p(p = 100, r = .25, i = 2)
+# comp_p(p = 100, r = .25, i = 4)
+
+
+# B: comp_ev: Number of events ----
+
+comp_ev <- function(ev = 0, r, i){
+
+  if (i == 0){ # stop:
+
+    # print(paste0(ev))
+
+  } else { # recurse:
+
+    ev <- c(comp_ev((ev + 1), r, (i - 1)), comp_ev(ev, r, (i - 1)))
+
+  }
+
+  return(ev)
+
+}
+
+# # Check:
+# comp_ev(ev = 0, r = .25, i = 2)
+# comp_ev(ev = 0, r = .25, i = 4)
+
+
+# C: comp_ev_p: Combination of comp_ev() and comp_p(): ----
+
+comp_ev_p <- function(p = 100, ev = 0, r, i){
+
+  out <- NA  # initialize
+
+  events <- comp_ev(ev, r, i) # A
+  cum_ps <- comp_p(p,   r, i) # B
+
+  # Combine output:
+  out <- cum_ps
+  names(out) <- paste0(events, "x")
+
+  return(out)
+
+}
+
+# Check:
+comp_ev_p(ev = 0, r = .25, i = 2)
+comp_ev_p(ev = 0, r = .25, i = 4)
 
 
 ## (*) Done: ----------
