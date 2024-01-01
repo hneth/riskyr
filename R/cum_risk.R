5005e45f3442b71995d5d4f72154e5db6e0cf5f8
hneth <hansneth@gmail.com>
2024-01-01 18:30:28 +0100

    add options (e.g., show_trans = 3), rename variables, and rename plot_cum_bar() to plot_cbar()

--- R/cum_risk.R
+++ R/cum_risk.R
@@ -165,12 +165,12 @@ comp_cum_ps <- function(r = 1/2,  # risk per time period
 
 
 
-# C. plot_cum_bar(): Compute and plot cum-risks (as barchart): ------
+# C. plot_cbar(): Compute and plot cumulative risks (as bar chart): ------
 
 
 #' Plot cumulative risk dynamics (as bar chart)
 #'
-#' plot_cum_bar plots the results of cumulative risk dynamics
+#' plot_cbar plots the results of cumulative risk dynamics
 #' as a bar chart (with percentages of risk event counts
 #' for each period t on a horizontal bar).
 #'
@@ -194,7 +194,7 @@ comp_cum_ps <- function(r = 1/2,  # risk per time period
 #' Default: \code{bar_width = .50}.
 #'
 #' @param show_trans numeric: Show transition polygons (between bars)?
-#' Values of 0/1/2 focus on no/new/remaining risk segments, respectively.
+#' Values of 0/1/2/3 focus on no/new/remaining/both risk segments, respectively.
 #' Default: \code{show_trans = 1} (i.e., focus on increasing risk segments).
 #'
 #' @param show_ev logical: Show number of risky event occurrence (as bar label)?
@@ -209,10 +209,10 @@ comp_cum_ps <- function(r = 1/2,  # risk per time period
 #' @importFrom grDevices colorRampPalette
 #'
 
-plot_cum_bar <- function(r = .50, t = 1, N = 100,
-                         horizontal = TRUE, sort = FALSE,
-                         N_max = 100, bar_width = .50,
-                         show_trans = 1, show_ev = TRUE, show_n = FALSE){
+plot_cbar <- function(r = .50, t = 1, N = 100,
+                      horizontal = TRUE, sort = FALSE,
+                      N_max = 100, bar_width = .50,
+                      show_trans = 1, show_ev = TRUE, show_n = FALSE){
 
 
   # Handle inputs: ----
@@ -230,8 +230,8 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
     show_ev <- FALSE
   }
 
-  if (show_trans %in% 0:2 == FALSE){
-    message("show_trans should be in 0:2: Setting shown_trans = 1 (i.e., focus on risk increase)")
+  if (show_trans %in% 0:3 == FALSE){
+    message("show_trans should be in 0:3: Setting shown_trans = 1 (i.e., focus on risk increase)")
     show_trans <- 1  # use default
   }
 
@@ -255,23 +255,15 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
 
   # Plot dimensions:
 
-  t_max <- t
-  y_max <- t_max + 1
+  t_max <- t + 1
 
 
   # Constants:
 
-  # bar_width  <- .50  # (from 0 to 1)
+  # bar labels:
+  cex_lbl <- 1 - (5 * t/100)
 
-  # show_trans <- TRUE # FALSE
-
-  # # bar labels:
-  # show_ev <- TRUE
-  # show_n  <- FALSE
-
-
-  cex_lbl <- 1 - (5 * t_max/100)
-
+  # Main axis label:
   if (N == 100){
     x_lbl <- "Percentage"
   } else if (N == 1){
@@ -279,7 +271,6 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
   } else {
     x_lbl <- "Frequency"
   }
-
 
   # Initialize plotting area:
 
@@ -289,7 +280,7 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
 
     plot(0:1, 0:1, type = "n",
          xlab = NA, ylab = NA,
-         xlim = c(0, N_max), ylim = c(0.5, y_max + 0.5),
+         xlim = c(0, N_max), ylim = c(0.5, t_max + 0.35),
          axes = FALSE)
 
     mtext(x_lbl, adj = .50, side = 1, line = 2)          # x-axis label
@@ -300,11 +291,11 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
 
     par(mar = c(3, 4, 4, 1) + 0.1)  # margins; default: par("mar") = 5.1 4.1 4.1 2.1.
 
-    y_max <- t_max + .50  # adjust
+    t_max <- t + .50  # adjust
 
     plot(0:1, 0:1, type = "n",
          xlab = NA, ylab = NA,
-         xlim = c(-0.5, y_max), ylim = c(0, N_max),
+         xlim = c(-0.5, t_max), ylim = c(0, N_max),
          axes = FALSE)
 
     mtext("Time period", adj = .50, side = 1, line = 1)  # x-axis label
@@ -335,10 +326,10 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
   col_lo  <- "grey98"
   col_hi  <- "firebrick" # "steelblue", "deepskyblue", "deeppink", "olivedrab", "grey20", "red3"
   brd_col <- "grey40"    # "white" # "grey20"
-  col_no  <- "forestgreen" # "deepskyblue"
+  col_no  <- "green4"    # "forestgreen" # "deepskyblue"
 
   # Color palette:
-  n_cols  <- 1 + t_max
+  n_cols  <- 1 + t
 
   # pal <- unikn::usecol(pal = c(col_lo, col_hi), n = n_cols, alpha = .80)
   pal <- grDevices::colorRampPalette(colors = c(col_lo, col_hi))(n_cols)
@@ -364,22 +355,22 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
   if (horizontal){ # horizontal bars:
 
     # Draw bar/box 0:
-    plot_cbox(x = N/2, y = y_max, lx = N, ly = bar_width,
+    plot_cbox(x = N/2, y = t_max, lx = N, ly = bar_width,
               lbl = lbl_0, cex = cex_lbl,
               col_fill = cur_col, col_brd = brd_col)
 
     # Add label (on left):
-    text(x = 0, y = y_max, labels = "0:", pos = 2, xpd = TRUE)
+    text(x = 0, y = t_max, labels = "0:", pos = 2, xpd = TRUE)
 
   } else { # vertical bars:
 
     # Draw bar/box 0:
-    plot_cbox(x = y_max - y_max, y = N/2, lx = bar_width, ly = N,
+    plot_cbox(x = 0, y = N/2, lx = bar_width, ly = N,
               lbl = lbl_0, cex = cex_lbl,
               col_fill = cur_col, col_brd = brd_col)
 
     # Add label (on top):
-    text(x = y_max - y_max, y = N_max + 5/N_max, labels = "0:", pos = 3, xpd = TRUE)
+    text(x = 0, y = N_max + 5/N_max, labels = "0:", pos = 3, xpd = TRUE)
 
   }
 
@@ -408,7 +399,7 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
     }
 
     x_prv <- 0  #  initialize x-store (previous x-value)
-    y_val <- y_max - t
+    cur_t <- t_max - t
 
     # For each p/ev-value in p_ev:
     for (i in 1:length(p_ev)){
@@ -435,13 +426,13 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
 
       # Compute transition values (for polygon segments):
 
-      if ((show_trans == 1) && (i %% 2 == 1)){ # odd segments (1, 3, 5...):
+      if ((show_trans == 1 | show_trans == 3) && (i %% 2 == 1)){ # odd segments (1, 3, 5...):
 
         x_top <- c(poly_cs[i], poly_cs[i])
         x_bot <- c(poly_cs[i + 1], poly_cs[i])
 
-        y_top <- c(y_max - (t - 1), y_max - (t - 1)) - bar_width/2
-        y_bot <- c(y_max - t, y_max - t) + bar_width/2
+        y_top <- c(t_max - (t - 1), t_max - (t - 1)) - bar_width/2
+        y_bot <- c(t_max - t, t_max - t) + bar_width/2
 
         xx <- c(x_top, x_bot)
         yy <- c(y_top, y_bot)
@@ -450,13 +441,13 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
 
       }
 
-      if ((show_trans == 2) && (i %% 2 == 0)){ # even segments (2, 4, 6...):
+      if ((show_trans == 2 | show_trans == 3) && (i %% 2 == 0)){ # even segments (2, 4, 6...):
 
         x_top <- c(poly_cs[i - 1], poly_cs[i + 1])
         x_bot <- c(poly_cs[i + 1], poly_cs[i])
 
-        y_top <- c(y_max - (t - 1), y_max - (t - 1)) - bar_width/2
-        y_bot <- c(y_max - t, y_max - t) + bar_width/2
+        y_top <- c(t_max - (t - 1), t_max - (t - 1)) - bar_width/2
+        y_bot <- c(t_max - t, t_max - t) + bar_width/2
 
         xx <- c(x_top, x_bot)
         yy <- c(y_top, y_bot)
@@ -471,16 +462,16 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
       if (horizontal){ # horizontal bars:
 
         # Draw transition:
-        if ((show_trans == 1) && (i %% 2 == 1)){ # odd segments (1, 3, 5...):
+        if ((show_trans == 1 | show_trans == 3) && (i %% 2 == 1)){ # odd segments (1, 3, 5...):
           polygon(x = xx, y = yy, col = pf_col, border = col_hi)
         }
 
-        if ((show_trans == 2) && (i %% 2 == 0)){ # even segments (2, 4, 6...):
+        if ((show_trans == 2 | show_trans == 3) && (i %% 2 == 0)){ # even segments (2, 4, 6...):
           polygon(x = xx, y = yy, col = pf_col, border = brd_col)
         }
 
         # Draw box:
-        plot_cbox(x = x_val, y = y_val, lx = x_width, ly = bar_width,
+        plot_cbox(x = x_val, y = cur_t, lx = x_width, ly = bar_width,
                   lbl = lbl_i, cex = cex_lbl,
                   col_fill = cur_col, col_brd = brd_col)
 
@@ -488,16 +479,16 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
       } else { # vertical bars:
 
         # Draw transition:
-        if ((show_trans == 1) && (i %% 2 == 1)){ # odd segments (1, 3, 5...):
-          polygon(x = y_max - yy, y = xx, col = pf_col, border = col_hi)
+        if ((show_trans == 1 | show_trans == 3) && (i %% 2 == 1)){ # odd segments (1, 3, 5...):
+          polygon(x = t_max - yy, y = xx, col = pf_col, border = col_hi)
         }
 
-        if ((show_trans == 2) && (i %% 2 == 0)){ # even segments (2, 4, 6...):
-          polygon(x = y_max - yy, y = xx, col = pf_col, border = brd_col)
+        if ((show_trans == 2 | show_trans == 3) && (i %% 2 == 0)){ # even segments (2, 4, 6...):
+          polygon(x = t_max - yy, y = xx, col = pf_col, border = brd_col)
         }
 
         # Draw box:
-        plot_cbox(x = y_max - y_val, y = x_val, lx = bar_width, ly = x_width,
+        plot_cbox(x = t_max - cur_t, y = x_val, lx = bar_width, ly = x_width,
                   lbl = lbl_i, cex = cex_lbl,
                   col_fill = cur_col, col_brd = brd_col)
 
@@ -512,12 +503,12 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
     if (horizontal){ # horizontal bars:
 
       # label on left:
-      text(x = 0, y = y_val, labels = paste0(t, ":"), pos = 2, xpd = TRUE)
+      text(x = 0, y = cur_t, labels = paste0(t, ":"), pos = 2, xpd = TRUE)
 
     } else { # vertical bars:
 
       # label on top:
-      text(x = y_max - y_val, y = N_max + 5/N_max, labels = paste0(t, ":"), pos = 3, xpd = TRUE)
+      text(x = t_max - cur_t, y = N_max + 5/N_max, labels = paste0(t, ":"), pos = 3, xpd = TRUE)
 
     }
 
@@ -527,7 +518,7 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
   # Title: ----
 
   # plot_title <- paste0("Cumulative risk dynamics (r = ", r, "; t = ", t, "; N = ", N, ")")
-  plot_title <- paste0("Cumulative risk dynamics (r = ", round(r, 2), ")")
+  plot_title <- paste0("Cumulative risks (r = ", round(r, 2), ")")
 
   title(main = plot_title, adj = 0)
 
@@ -536,56 +527,56 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
 
   return(invisible(data))  # return data
 
-} # plot_cum_bar().
+} # plot_cbar().
 
 
 # # # Check:
-# plot_cum_bar()
+# plot_cbar()
 #
-# plot_cum_bar(r = .25, t = 4, N = 100)
-# plot_cum_bar(r = .25, t = 4, N = 100, show_n = TRUE)
-# plot_cum_bar(r = .25, t = 4, N = 100, sort = TRUE)  # sorting
-# plot_cum_bar(r = .25, t = 4, N = 100, horizontal = FALSE)  # vertical bars
-# plot_cum_bar(r = .25, t = 5, N = 100, show_trans = FALSE)  # only bars
-# plot_cum_bar(r = .25, t = 5, N = 100, bar_width = 1)  # only bars
-# plot_cum_bar(r = .25, t = 5, N = 100, bar_width = 0)  # no bars
+# plot_cbar(r = .25, t = 4, N = 100)
+# plot_cbar(r = .25, t = 4, N = 100, show_n = TRUE)
+# plot_cbar(r = .25, t = 4, N = 100, sort = TRUE)  # sorting
+# plot_cbar(r = .25, t = 4, N = 100, horizontal = FALSE)  # vertical bars
+# plot_cbar(r = .25, t = 5, N = 100, show_trans = FALSE)  # only bars
+# plot_cbar(r = .25, t = 5, N = 100, bar_width = 1)  # only bars
+# plot_cbar(r = .25, t = 5, N = 100, bar_width = 0)  # no bars
 
-# plot_cum_bar(r = .50, t = 3, N = 100)
-# plot_cum_bar(r = .50, t = 3, N = 100, sort = TRUE)  # sorting
-# plot_cum_bar(r = .50, t = 3, N = 100, horizontal = FALSE)  # sorting
+# plot_cbar(r = .50, t = 3, N = 100)
+# plot_cbar(r = .50, t = 3, N = 100, sort = TRUE)  # sorting
+# plot_cbar(r = .50, t = 3, N = 100, horizontal = FALSE)  # sorting
 #
-# plot_cum_bar(r = .75, t = 4, N = 100)
-# plot_cum_bar(r = .75, t = 4, N = 100, sort = TRUE)  # sorting
-# plot_cum_bar(r = .75, t = 4, N = 100, horizontal = FALSE)  # vertical bars
+# plot_cbar(r = .75, t = 4, N = 100)
+# plot_cbar(r = .75, t = 4, N = 100, sort = TRUE)  # sorting
+# plot_cbar(r = .75, t = 4, N = 100, horizontal = FALSE)  # vertical bars
 #
-# plot_cum_bar(r = .05, t = 5, N_max = 25)  # zooming in
-# plot_cum_bar(r = .05, t = 5, sort = TRUE, N_max = 1)  # sorting & zooming in
+# plot_cbar(r = .05, t = 5, N_max = 25)  # zooming in
+# plot_cbar(r = .05, t = 5, sort = TRUE, N_max = 1)  # sorting & zooming in
 
 # # Problems from McCloy, Byrne, & Johnson-Laird (2010): p. 508
 # # 1. Pregnancy (10%/90%), conjunctive: 1/2/3 year, disjunctive 3 year:
-# plot_cum_bar(r = .10, t = 3, N = 1000, N_max = 1000)
+# plot_cbar(r = .10, t = 3, N = 1000, N_max = 1000)
 # # 2. Kapi fruit (20%/80%), conjunctive: 1/2/3 year, disjunctive 3 year:
-# plot_cum_bar(r = .20, t = 3, N = 1000, N_max = 1000)
+# plot_cbar(r = .20, t = 3, N = 1000, N_max = 1000)
 # # 3. Eye infection (30%/70%), conjunctive: 1/2/3 year, disjunctive 3 year:
-# plot_cum_bar(r = .30, t = 3, N = 1000, N_max = 1000)
+# plot_cbar(r = .30, t = 3, N = 1000, N_max = 1000)
 
 
 # # Note some insights (from visualizations):
 #
 # # 1. Small cumulative risks (r < .10) behave almost additively/linearly:
-# plot_cum_bar(r = .01, t = 5, sort = F, N_max =  5)
-# plot_cum_bar(r = .05, t = 5, sort = F, N_max = 25)
-# plot_cum_bar(r = .10, t = 5, sort = F, N_max = 50)
+# plot_cbar(r = .01, t = 5, sort = F, N_max =  5)
+# plot_cbar(r = .05, t = 5, sort = F, N_max = 25)
+# plot_cbar(r = .10, t = 5, sort = F, N_max = 50)
 #
 # # 2. Large cumulative risks (r > .40) rapidly affect the entire population:
-# plot_cum_bar(r = .40, t = 5, sort = F, N_max = 100)
-# plot_cum_bar(r = .50, t = 5, sort = F, N_max = 100)
-# plot_cum_bar(r = .60, t = 5, sort = F, N_max = 100)
-# plot_cum_bar(r = .70, t = 5, sort = F, N_max = 100)
+# plot_cbar(r = .40, t = 5, sort = F, N_max = 100)
+# plot_cbar(r = .50, t = 5, sort = F, N_max = 100)
+# plot_cbar(r = .60, t = 5, sort = F, N_max = 100)
+# plot_cbar(r = .70, t = 5, sort = F, N_max = 100)
 #
 # # 3. => Intermediate range (.10 <= r <= .40) is crucial/problematic: sub-additive
-# plot_cum_bar(r = .20, t = 5, sort = F, N_max = 100)
-# plot_cum_bar(r = .30, t = 5, sort = F, N_max = 100)
+# plot_cbar(r = .20, t = 5, sort = F, N_max = 100)
+# plot_cbar(r = .30, t = 5, sort = F, N_max = 100)
 
 
 # ?: +++ here now +++
@@ -609,7 +600,7 @@ plot_cum_bar <- function(r = .50, t = 1, N = 100,
 
 ## (+) ToDo: ----------
 
-# - make bar_width and show_trans arguments of plot_cum_bar().
+# - make bar_width and show_trans arguments of plot_cbar().
 # - rename x and y variables: x in terms of p, y in terms of t, etc.
 
 # - Use more appropriate data structure for cumulative probabilities (ps)?
