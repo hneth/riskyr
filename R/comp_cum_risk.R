5531e5060f1b7390b2a74c3f2563e4a30bfabd68
hneth <hansneth@gmail.com>
2024-01-02 08:42:57 +0100

    split cum_risk.R into 2 files: comp_cum_risk.R and plot_cum_risk.R

---
+++ R/comp_cum_risk.R
@@ -0,0 +1,190 @@
+## comp_cum_risk.R | riskyr
+## 2024 01 02
+## Compute cumulative risks
+
+# Parameters: ----
+
+# r ... risk
+# i ... periods/rounds/times
+#
+# ev ... events
+# pc ... percent/population
+
+
+# A. Recursive application of repeated risk r: ------
+
+# 1. apply_risk() as recursive partition of population pc: ----
+
+apply_risk <- function(pc = 100, ev = 0, r, i){
+
+  # cat(i, ": ", pc, "\n")
+
+
+  if (i == 0){ # stop:
+
+    # print(paste0(ev, ": ", pc))
+
+  } else { # recurse:
+
+    j <- (i - 1)  # simplify
+
+    pc <- c(apply_risk(pc * r, ev + 1, r, j), apply_risk(pc * (1 - r), ev, r, j))
+
+  }
+
+  # Output:
+  return(pc)
+
+}
+
+# # Check:
+# apply_risk(pc = 100, ev = 0, r = .25, i = 4)
+
+
+# 2. Separately for p and ev: ------
+
+# - a: comp_p: Probabilities ----
+
+comp_p <- function(p = 100, r, i){
+
+  if (i == 0){ # stop:
+
+    # print(paste0(p))
+
+  } else { # recurse:
+
+    p <- c(comp_p(p * r, r, (i - 1)), comp_p(p * (1 - r), r, (i - 1)))
+
+  }
+
+  return(p)
+
+}
+
+# # Check:
+# comp_p(p = 100, r = .25, i = 2)
+# comp_p(p = 100, r = .25, i = 4)
+
+
+# - b: comp_ev: Number of events ----
+
+comp_ev <- function(ev = 0, r, i){
+
+  if (i == 0){ # stop:
+
+    # print(paste0(ev))
+
+  } else { # recurse:
+
+    ev <- c(comp_ev((ev + 1), r, (i - 1)), comp_ev(ev, r, (i - 1)))
+
+  }
+
+  return(ev)
+
+}
+
+# # Check:
+# comp_ev(ev = 0, r = .25, i = 2)
+# comp_ev(ev = 0, r = .25, i = 4)
+
+
+# - c: comp_ev_p: Combine comp_ev() and comp_p() ----
+
+comp_ev_p <- function(p = 100, ev = 0, r, i){
+
+  out <- NA  # initialize
+
+  # Compute (using recursive functions):
+  events <- comp_ev(ev, r, i) # A
+  cum_ps <- comp_p(p,   r, i) # B
+
+  # Combine outputs:
+  out <- cum_ps
+  names(out) <- paste0(events, "x")
+
+  return(out)
+
+}
+
+# # Check:
+# comp_ev_p(p = 100, ev = 0, r = .25, i = 1)
+# comp_ev_p(p = 100, ev = 0, r = .25, i = 2)
+# comp_ev_p(p = 100, ev = 0, r = .25, i = 3)
+# comp_ev_p(p = 100, ev = 0, r = .25, i = 4)
+
+
+
+# B. comp_cum_ps(): Iterative generation of cumulative risks: ------
+
+comp_cum_ps <- function(r = 1/2,  # risk per time period
+                        t = 1,    # time periods/rounds
+                        N = 100   # population size
+){
+
+  # iterative generation:
+  for (i in 0:t){ # each period i:
+
+    if (i == 0){
+
+      # prepare:
+      ev <- vector(mode = "list", length = t)
+      ps <- vector(mode = "list", length = t)
+
+    } else if (i == 1){
+
+      # initialize:
+      ev[[i]] <- c(1, 0)
+      ps[[i]] <- c(N * r, N * (1 - r))
+
+      names(ps[[i]]) <- paste0(ev[[i]], "x")
+
+    } else {
+
+      for (e in 1:length(ev[[i - 1]])){ # each element e in the previous ev vector:
+
+        ev[[i]][c((2 * e - 1), 2 * e)] <- ev[[i - 1]][e] + c(1, 0)
+        ps[[i]][c((2 * e - 1), 2 * e)] <- ps[[i - 1]][e] * c(r, (1 - r))
+
+        names(ps[[i]]) <- paste0(ev[[i]], "x")
+
+      } # for e.
+
+    } # if i.
+
+  } # for i.
+
+  # Output:
+  return(ps)
+
+} # comp_cum_ps().
+
+# # Check:
+# comp_cum_ps()
+# comp_cum_ps(.1, 5, 100)
+
+
+
+
+# See file "comp_cum_risk.R".
+
+# C. plot_cbar(): Compute and plot cumulative risks (as bar chart): ------
+
+# See file "plot_cum_risk.R".
+
+# ?: +++ here now +++
+
+
+## (*) Done: ----------
+
+# - etc.
+
+
+## (+) ToDo: ----------
+
+
+# - Use more appropriate data structure for cumulative probabilities (ps)?
+# - How to grow a (binary) tree structure in R?
+
+
+## eof. ------------------------------------------
--- R/plot_cum_risk.R
+++ R/plot_cum_risk.R
@@ -1,176 +1,18 @@
-## cum_risk.R | riskyr
+## plot_cum_risk.R | riskyr
 ## 2024 01 02
-## Compute and plot cumulative risks
+## Plot cumulative risks
 
-# Parameters: ----
+# Compute cumulative risks: ------
 
-# r ... risk
-# i ... periods/rounds/times
-#
-# ev ... events
-# pc ... percent/population
+# See file "comp_cum_risk.R".
 
 
-# A. Recursive application of repeated risk r: ------
-
-# 1. apply_risk() as recursive partition of population pc: ----
-
-apply_risk <- function(pc = 100, ev = 0, r, i){
-
-  # cat(i, ": ", pc, "\n")
-
-
-  if (i == 0){ # stop:
-
-    # print(paste0(ev, ": ", pc))
-
-  } else { # recurse:
-
-    j <- (i - 1)  # simplify
-
-    pc <- c(apply_risk(pc * r, ev + 1, r, j), apply_risk(pc * (1 - r), ev, r, j))
-
-  }
-
-  # Output:
-  return(pc)
-
-}
-
-# # Check:
-# apply_risk(pc = 100, ev = 0, r = .25, i = 4)
-
-
-# 2. Separately for p and ev: ------
-
-# - a: comp_p: Probabilities ----
-
-comp_p <- function(p = 100, r, i){
-
-  if (i == 0){ # stop:
-
-    # print(paste0(p))
-
-  } else { # recurse:
-
-    p <- c(comp_p(p * r, r, (i - 1)), comp_p(p * (1 - r), r, (i - 1)))
-
-  }
-
-  return(p)
-
-}
-
-# # Check:
-# comp_p(p = 100, r = .25, i = 2)
-# comp_p(p = 100, r = .25, i = 4)
-
-
-# - b: comp_ev: Number of events ----
-
-comp_ev <- function(ev = 0, r, i){
-
-  if (i == 0){ # stop:
-
-    # print(paste0(ev))
-
-  } else { # recurse:
-
-    ev <- c(comp_ev((ev + 1), r, (i - 1)), comp_ev(ev, r, (i - 1)))
-
-  }
-
-  return(ev)
-
-}
-
-# # Check:
-# comp_ev(ev = 0, r = .25, i = 2)
-# comp_ev(ev = 0, r = .25, i = 4)
-
-
-# - c: comp_ev_p: Combine comp_ev() and comp_p() ----
-
-comp_ev_p <- function(p = 100, ev = 0, r, i){
-
-  out <- NA  # initialize
-
-  # Compute (using recursive functions):
-  events <- comp_ev(ev, r, i) # A
-  cum_ps <- comp_p(p,   r, i) # B
-
-  # Combine outputs:
-  out <- cum_ps
-  names(out) <- paste0(events, "x")
-
-  return(out)
-
-}
-
-# # Check:
-# comp_ev_p(p = 100, ev = 0, r = .25, i = 1)
-# comp_ev_p(p = 100, ev = 0, r = .25, i = 2)
-# comp_ev_p(p = 100, ev = 0, r = .25, i = 3)
-# comp_ev_p(p = 100, ev = 0, r = .25, i = 4)
-
-
-
-# B. comp_cum_ps(): Iterative generation of cumulative risks: ------
-
-comp_cum_ps <- function(r = 1/2,  # risk per time period
-                        t = 1,    # time periods/rounds
-                        N = 100   # population size
-){
-
-  # iterative generation:
-  for (i in 0:t){ # each period i:
-
-    if (i == 0){
-
-      # prepare:
-      ev <- vector(mode = "list", length = t)
-      ps <- vector(mode = "list", length = t)
-
-    } else if (i == 1){
-
-      # initialize:
-      ev[[i]] <- c(1, 0)
-      ps[[i]] <- c(N * r, N * (1 - r))
-
-      names(ps[[i]]) <- paste0(ev[[i]], "x")
-
-    } else {
-
-      for (e in 1:length(ev[[i - 1]])){ # each element e in the previous ev vector:
-
-        ev[[i]][c((2 * e - 1), 2 * e)] <- ev[[i - 1]][e] + c(1, 0)
-        ps[[i]][c((2 * e - 1), 2 * e)] <- ps[[i - 1]][e] * c(r, (1 - r))
-
-        names(ps[[i]]) <- paste0(ev[[i]], "x")
-
-      } # for e.
-
-    } # if i.
-
-  } # for i.
-
-  # Output:
-  return(ps)
-
-} # comp_cum_ps().
-
-# # Check:
-# comp_cum_ps()
-# comp_cum_ps(.1, 5, 100)
-
-
-
-# C. plot_cbar(): Compute and plot cumulative risks (as bar chart): ------
+# plot_cbar(): Compute and plot cumulative risks (as bar chart): ------
 
 
 #' Plot cumulative risk dynamics (as bar chart)
 #'
-#' plot_cbar plots the results of cumulative risk dynamics
+#' \code{plot_cbar} plots the results of cumulative risk dynamics
 #' as a bar chart (with percentages of risk event counts
 #' for each period t on a horizontal bar).
 #'
@@ -582,6 +424,7 @@ plot_cbar <- function(r = .50, t = 1, N = 100,
 # ?: +++ here now +++
 
 
+
 ## (*) Done: ----------
 
 # - Added option to sort cum-risk vectors by names:
@@ -597,14 +440,13 @@ plot_cbar <- function(r = .50, t = 1, N = 100,
 #   (in addition to ev-occurrences)
 #
 # - Added transition links between time periods (as polygons)
+#
+# - Made bar_width and show_trans arguments of plot_cbar().
+
 
 ## (+) ToDo: ----------
 
-# - make bar_width and show_trans arguments of plot_cbar().
 # - rename x and y variables: x in terms of p, y in terms of t, etc.
-
-# - Use more appropriate data structure for cumulative probabilities (ps)?
-# - How to grow a (binary) tree structure in R?
 
 
 ## eof. ------------------------------------------
